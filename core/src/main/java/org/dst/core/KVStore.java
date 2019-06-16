c28c8013cffc553a1174fa1282639e7201d1ebef
jinyuxin <810916965@qq.com>
2019-06-16 15:59:23 +0800

    modified KVStore.java

--- core/src/main/java/org/dst/core/KVStore.java
+++ core/src/main/java/org/dst/core/KVStore.java
@@ -1,6 +1,10 @@
 package org.dst.core;
 
-import org.dst.core.operatorset.*;
+import org.dst.core.operatorset.DstList;
+import org.dst.core.operatorset.DstSet;
+import org.dst.core.operatorset.DstString;
+import org.dst.core.operatorset.DstTable;
+import org.dst.core.operatorset.DstDict;
 
 /**
  * The api of kv-store.
