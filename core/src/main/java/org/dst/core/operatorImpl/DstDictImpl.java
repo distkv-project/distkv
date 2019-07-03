06026265892ca2aaf0059c19f4c697fcf0f181e2
Samuel King <810916965@qq.com>
2019-07-04 00:09:31 +0800

    add test module and left valuetype (#21)

--- core/src/main/java/org/dst/core/operatorImpl/DstDictImpl.java
+++ core/src/main/java/org/dst/core/operatorImpl/DstDictImpl.java
@@ -6,29 +6,29 @@ import java.util.Map;
 
 public class DstDictImpl implements DstDict {
 
-    private HashMap<String, Map<String, String>> dictMap;
+  private HashMap<String, Map<String, String>> dictMap;
 
-    public DstDictImpl() {
-        this.dictMap = new HashMap<String, Map<String, String>>();
+  public DstDictImpl() {
+    this.dictMap = new HashMap<String, Map<String, String>>();
+  }
+
+  @Override
+  public void put(String Key, Map<String, String> Value) {
+    dictMap.put(Key, Value);
+  }
+
+  @Override
+  public Map<String, String> get(String Key) {
+    return dictMap.get(Key);
+  }
+
+  @Override
+  public boolean del(String Key) {
+    if (!dictMap.containsKey(Key)) {
+      return false;
     }
 
-    @Override
-    public void put(String Key, Map<String, String> Value) {
-        dictMap.put(Key, Value);
-    }
-
-    @Override
-    public Map<String, String> get(String Key) {
-        return dictMap.get(Key);
-    }
-
-    @Override
-    public boolean del(String Key) {
-        if (!dictMap.containsKey(Key)) {
-            return false;
-        }
-
-        dictMap.remove(Key);
-        return true;
-    }
+    dictMap.remove(Key);
+    return true;
+  }
 }
--- core/src/main/java/org/dst/core/operatorImpl/DstTableImpl.java
+++ core/src/main/java/org/dst/core/operatorImpl/DstTableImpl.java
@@ -1,6 +1,19 @@
 package org.dst.core.operatorImpl;
 
+import org.dst.core.TableSpecification;
+import org.dst.core.RecordEntry;
 import org.dst.core.operatorset.DstTable;
 
 public class DstTableImpl implements DstTable {
+
+  @Override
+  public void createTable(TableSpecification tableSpec) {
+
+  }
+
+  @Override
+  public void append(RecordEntry recordEntry) {
+
+  }
+
 }
