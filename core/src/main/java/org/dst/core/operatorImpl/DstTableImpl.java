f59dc9069bf069a5d48417640ef272b1ff353098
jinyuxin <810916965@qq.com>
2019-07-04 21:55:25 +0800

    throw notImplementException

--- core/src/main/java/org/dst/core/operatorImpl/DstTableImpl.java
+++ core/src/main/java/org/dst/core/operatorImpl/DstTableImpl.java
@@ -1,5 +1,6 @@
 package org.dst.core.operatorImpl;
 
+import org.dst.core.exception.NotImplementException;
 import org.dst.core.operatorset.DstTable;
 import org.dst.core.RecordEntry;
 import org.dst.core.TableSpecification;
@@ -7,11 +8,11 @@ import org.dst.core.TableSpecification;
 public class DstTableImpl implements DstTable {
   @Override
   public void createTable(TableSpecification tableSpec) {
-
+    throw new NotImplementException();
   }
 
   @Override
   public void append(RecordEntry recordEntry) {
-
+    throw new NotImplementException();
   }
 }
