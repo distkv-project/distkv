901aba491b265959814bdb385302ac56ff8cafa1
Samuel King <810916965@qq.com>
2019-07-05 10:52:48 +0800

    Add test-module (#22)
    * add test-module
    
    * add a new line
    
    * throw notImplementException
    
    * deal with some problem

--- core/src/main/java/org/dst/core/operatorImpl/DstTableImpl.java
+++ core/src/main/java/org/dst/core/operatorImpl/DstTableImpl.java
@@ -1,19 +1,18 @@
 package org.dst.core.operatorImpl;
 
-import org.dst.core.TableSpecification;
-import org.dst.core.RecordEntry;
+import org.dst.core.exception.NotImplementException;
 import org.dst.core.operatorset.DstTable;
+import org.dst.core.RecordEntry;
+import org.dst.core.TableSpecification;
 
 public class DstTableImpl implements DstTable {
-
   @Override
   public void createTable(TableSpecification tableSpec) {
-
+    throw new NotImplementException();
   }
 
   @Override
   public void append(RecordEntry recordEntry) {
-
+    throw new NotImplementException();
   }
-
 }
