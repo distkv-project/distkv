dd35ed513aa54894e0fcca188fca7cc61d8e5978
Samuel King <810916965@qq.com>
2019-07-05 11:08:30 +0800

    Implement other kinds of values (#23)
    * implement other kinds of values
    
    * change getValue() return type in RawDataValue.java
    
    * change to return value

--- core/src/main/java/org/dst/core/operatorImpl/DstTableImpl.java
+++ core/src/main/java/org/dst/core/operatorImpl/DstTableImpl.java
@@ -1,9 +1,9 @@
 package org.dst.core.operatorImpl;
 
+import org.dst.core.TableSpecification;
+import org.dst.core.RecordEntry;
 import org.dst.core.exception.NotImplementException;
 import org.dst.core.operatorset.DstTable;
-import org.dst.core.RecordEntry;
-import org.dst.core.TableSpecification;
 
 public class DstTableImpl implements DstTable {
   @Override
