859480b327e1cdea9046c320bd77ba7b10a03e9e
jinyuxin <810916965@qq.com>
2019-07-28 11:55:20 +0800

    change logger to LOGGER

--- test/src/main/java/test/org/dst/logback/LogbackTest.java
+++ test/src/main/java/test/org/dst/logback/LogbackTest.java
@@ -6,15 +6,15 @@ import org.slf4j.LoggerFactory;
 
 public class LogbackTest {
 
-    private static final Logger logger = LoggerFactory.getLogger(LogbackTest.class.getName());
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogbackTest.class.getName());
 
     @Test
     public void testLogback() {
-        logger.info("This is info message");
-        logger.debug("This is debug message");
-        logger.error("This is error message");
-        logger.trace("This is trace message");
-        logger.warn("This is warn message");
+        LOGGER.info("This is info message");
+        LOGGER.debug("This is debug message");
+        LOGGER.error("This is error message");
+        LOGGER.trace("This is trace message");
+        LOGGER.warn("This is warn message");
     }
 
 }
