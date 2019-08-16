af02b7a9e67de4fb2ad5bedefdcec19cf35e1abc
senyer <576085903@qq.com>
2019-08-17 02:48:17 +0800

    test for implements rput、lput、rdel、ldel method of kvstore-list()

--- test/src/test/java/test/org/dst/core/KVStoreTest.java
+++ test/src/test/java/test/org/dst/core/KVStoreTest.java
@@ -1,11 +1,7 @@
 package test.org.dst.core;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.HashSet;
+import java.util.*;
+
 import org.dst.core.KVStoreImpl;
 import org.dst.core.KVStore;
 import org.dst.core.exception.KeyNotFoundException;
@@ -33,9 +29,50 @@ public class KVStoreTest {
     list.add("v2");
     list.add("v3");
     ks.list().put("k1", list);
+
+    //get
     Assertions.assertEquals(list, ks.list().get("k1"));
+    //del
     Assertions.assertTrue(ks.list().del("k1"));
     Assertions.assertNull(ks.list().get("k1"));
+
+    //lput
+    List<String> list1 = new ArrayList<String>();
+    list1.add("v1");
+    ks.list().put("k1",list1);
+    List<String> list2 = new ArrayList<>();
+    list2.add("v2");
+    ks.list().lput("k1",list2);
+    String[] str={"v2","v1"};
+    Assertions.assertEquals(Arrays.asList(str),ks.list().get("k1"));
+
+    //rput
+    List<String> list3 = new ArrayList<>();
+    list3.add("v3");
+    ks.list().rput("k1", list3);
+    String[] str1={"v2","v1","v3"};
+    Assertions.assertEquals(Arrays.asList(str1),ks.list().get("k1"));
+
+    //ldel
+    ks.list().ldel("k1",1);
+    String[] str2={"v1","v3"};
+    Assertions.assertEquals(Arrays.asList(str2),ks.list().get("k1"));
+    Assertions.assertFalse(ks.list().ldel("-k",1));
+
+    //rdel
+    ks.list().rdel("k1",1);
+    String[] str3={"v1"};
+    Assertions.assertEquals(Arrays.asList(str3),ks.list().get("k1"));
+    Assertions.assertFalse(ks.list().rdel("-k",1));
+  }
+
+  @Test
+  public void testLputList() {
+    KVStore ks = new KVStoreImpl();
+    List<String> list = new ArrayList<String>();
+    list.add("v1");
+    ks.list().put("k1",list);
+
   }
 
   @Test
