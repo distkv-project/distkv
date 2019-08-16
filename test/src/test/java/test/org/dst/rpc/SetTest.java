e1b863e6a2dcf40afbb88c85c014b88f050a552e
kairbon <wangkai31415926@gmail.com>
2019-08-16 12:08:22 +0800

    Refine rpc test

---
+++ test/src/test/java/test/org/dst/rpc/SetTest.java
@@ -0,0 +1,42 @@
+package test.org.dst.rpc;
+
+import com.google.common.collect.ImmutableList;
+import junit.framework.Assert;
+import org.dst.server.generated.SetProtocol;
+import org.dst.server.service.DstSetService;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+public class SetTest {
+
+    @Test
+    public void testSetRpcCall() {
+
+        DstSetService setService = TestUtil.openConnection(DstSetService.class);
+
+        // Test set put.
+        SetProtocol.SetPutRequest.Builder setPutRequestBuilder =
+                SetProtocol.SetPutRequest.newBuilder();
+        setPutRequestBuilder.setKey("k1");
+        final List<String> values = ImmutableList.of("v1", "v2", "v3","v1");
+        values.forEach(value -> setPutRequestBuilder.addValues(value));
+
+        SetProtocol.SetPutResponse setPutResponse =
+                setService.setPut(setPutRequestBuilder.build());
+        Assert.assertEquals("ok", setPutResponse.getStatus());
+
+        // Test set get.
+        SetProtocol.SetGetRequest.Builder setGetRequestBuilder =
+                SetProtocol.SetGetRequest.newBuilder();
+        setGetRequestBuilder.setKey("k1");
+
+        SetProtocol.SetGetResponse setGetResponse =
+                setService.setGet(setGetRequestBuilder.build());
+
+        final List<String> results = ImmutableList.of("v1", "v2", "v3");
+        Assert.assertEquals("ok", setGetResponse.getStatus());
+        Assert.assertEquals(results, setGetResponse.getValuesList());
+
+        TestUtil.closeConnection();
+    }
+}
--- test/src/test/java/test/org/dst/rpc/TestUtil.java
+++ test/src/test/java/test/org/dst/rpc/TestUtil.java
@@ -3,7 +3,13 @@ package test.org.dst.rpc;
 import java.io.File;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import com.baidu.brpc.client.BrpcProxy;
+import com.baidu.brpc.client.RpcClient;
+import com.baidu.brpc.client.RpcClientOptions;
+import com.baidu.brpc.protocol.Options;
 import com.google.common.collect.ImmutableList;
+import org.dst.server.service.DstStringService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +22,7 @@ public class TestUtil {
 
   private static Process rpcServerProcess = null;
 
+  private static RpcClient client;
   /**
    * @param command the command to start rpc server using a new process.
    */
@@ -50,6 +57,24 @@ public class TestUtil {
 
   public static void stopRpcServer() {
     rpcServerProcess.destroy();
+  }
+
+  public static <T> T openConnection(Class classz){
+    startRpcServer();
+    RpcClientOptions options = new RpcClientOptions();
+    options.setProtocolType(Options.ProtocolType.PROTOCOL_BAIDU_STD_VALUE);
+    options.setWriteTimeoutMillis(1000);
+    options.setReadTimeoutMillis(1000);
+    options.setMaxTotalConnections(1000);
+    options.setMinIdleConnections(10);
+    final String url = "list://127.0.0.1:8082";
+    client = new RpcClient(url, options);
+    return BrpcProxy.getProxy(client, classz);
+  }
+
+  public static void closeConnection(){
+    client.stop();
+    TestUtil.stopRpcServer();
   }
 }
 
