761d8f2508e0b8c0c83b5cade18f746a15d9bbe7
kairbon <wangkai31415926@gmail.com>
2019-08-16 15:06:43 +0800

    update

--- test/src/test/java/test/org/dst/rpc/ListTest.java
+++ test/src/test/java/test/org/dst/rpc/ListTest.java
@@ -3,6 +3,8 @@ package test.org.dst.rpc;
 import com.google.common.collect.ImmutableList;
 import junit.framework.Assert;
 import org.dst.server.generated.DstServerProtocol;
+import org.dst.server.generated.ListProtocol;
+import org.dst.server.service.DstListService;
 import org.dst.server.service.DstStringService;
 import org.junit.jupiter.api.Test;
 
@@ -13,24 +15,25 @@ public class ListTest {
     @Test
     public void testListRpcCall() {
 
-        ProxyOnClient<DstStringService> setProxy = new ProxyOnClient(DstStringService.class);
-        DstStringService listService = setProxy.openConnection();
-                // Test list put.
-        DstServerProtocol.ListPutRequest.Builder putRequestBuilder =
-                DstServerProtocol.ListPutRequest.newBuilder();
+        ProxyOnClient<DstListService> setProxy = new ProxyOnClient(DstListService.class);
+        DstListService listService = setProxy.openConnection();
+
+        // Test list put.
+        ListProtocol.ListPutRequest.Builder putRequestBuilder =
+                ListProtocol.ListPutRequest.newBuilder();
         putRequestBuilder.setKey("k1");
         final List<String> values = ImmutableList.of("v1", "v2", "v3");
         values.forEach(value -> putRequestBuilder.addValues(value));
 
-        DstServerProtocol.ListPutResponse listPutResponse =
+        ListProtocol.ListPutResponse listPutResponse =
                 listService.listPut(putRequestBuilder.build());
         Assert.assertEquals("ok", listPutResponse.getStatus());
 
         // Test list get.
-        DstServerProtocol.ListGetRequest.Builder getRequestBuilder =
-                DstServerProtocol.ListGetRequest.newBuilder();
+        ListProtocol.ListGetRequest.Builder getRequestBuilder =
+                ListProtocol.ListGetRequest.newBuilder();
         getRequestBuilder.setKey("k1");
-        DstServerProtocol.ListGetResponse listGetResponse =
+        ListProtocol.ListGetResponse listGetResponse =
                 listService.listGet(getRequestBuilder.build());
 
         Assert.assertEquals("ok", listGetResponse.getStatus());
--- test/src/test/java/test/org/dst/rpc/ServerTest.java
+++ test/src/test/java/test/org/dst/rpc/ServerTest.java
@@ -1,6 +1,7 @@
 package test.org.dst.rpc;
 
 import org.dst.server.generated.DstServerProtocol;
+import org.dst.server.generated.StringProtocol;
 import org.dst.server.service.DstStringService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -13,22 +14,22 @@ public class ServerTest {
         DstStringService stringService = setProxy.openConnection();
 
         // Test string put request
-        DstServerProtocol.StringPutRequest stringPutRequest =
-                DstServerProtocol.StringPutRequest.newBuilder()
+        StringProtocol.StringPutRequest stringPutRequest =
+                StringProtocol.StringPutRequest.newBuilder()
                         .setKey("k1")
                         .setValue("v1")
                         .build();
 
-        DstServerProtocol.StringPutResponse stringResponse = stringService.strPut(stringPutRequest);
+        StringProtocol.StringPutResponse stringResponse = stringService.strPut(stringPutRequest);
         Assertions.assertEquals("ok", stringResponse.getStatus());
 
         // Test string get request
-        DstServerProtocol.StringGetRequest strGetRequest =
-                DstServerProtocol.StringGetRequest.newBuilder()
+        StringProtocol.StringGetRequest strGetRequest =
+                StringProtocol.StringGetRequest.newBuilder()
                         .setKey("k1")
                         .build();
 
-        DstServerProtocol.StringGetResponse stringGetRequest = stringService.strGet(strGetRequest);
+        StringProtocol.StringGetResponse stringGetRequest = stringService.strGet(strGetRequest);
         Assertions.assertEquals("v1", stringGetRequest.getValue());
 
         setProxy.closeConnection();
