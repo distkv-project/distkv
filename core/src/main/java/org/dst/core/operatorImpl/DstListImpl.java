9f4bd53dccb46af6e7fe8d6fd2eedee2e06a856b
senyer <576085903@qq.com>
2019-08-17 02:45:23 +0800

    implements rput、lput、rdel、ldel method of kvstore-list()

--- core/src/main/java/org/dst/core/operatorImpl/DstListImpl.java
+++ core/src/main/java/org/dst/core/operatorImpl/DstListImpl.java
@@ -1,11 +1,15 @@
 package org.dst.core.operatorImpl;
 
-import org.dst.core.exception.NotImplementException;
 import org.dst.core.operatorset.DstList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.List;
 
 public class DstListImpl implements DstList {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(DstListImpl.class.getName());
 
   private HashMap<String, List<String>> listMap;
 
@@ -35,21 +39,53 @@ public class DstListImpl implements DstList {
 
   @Override
   public boolean lput(String key, List<String> value) {
-    throw new NotImplementException("The method is not implemented");
+      if (listMap.containsKey(key)) {
+        value.addAll(listMap.get(key));
+      }
+      return listMap.put(key, value).size()>0;
   }
 
   @Override
   public boolean rput(String key, List<String> value) {
-    throw new NotImplementException("The method is not implemented");
+    if (listMap.containsKey(key)) {
+      return listMap.get(key).addAll(value);
+    }
+    return listMap.put(key, value).size()>0;
   }
 
   @Override
   public boolean ldel(String key, int n) {
-    throw new NotImplementException("The method is not implemented");
+    if (listMap.containsKey(key)) {
+      List<String> original = listMap.get(key);
+      if(original.size()<n){
+        original.clear();
+        return true;
+      }
+      for (int i = 0; i < n; i++) {
+        original.remove(0);
+      }
+      return true;
+    }
+    LOGGER.info(" no key find to ldel for kvstore-list()");
+    return false;
   }
 
   @Override
   public boolean rdel(String key, int n) {
-    throw new NotImplementException("The method is not implemented");
+    if (listMap.containsKey(key)) {
+      List<String> original = listMap.get(key);
+      int size = original.size();
+      if(size<=n){
+        original.clear();
+        return true;
+      }
+      for (int i = 0; i < n; i++) {
+        original.remove(size-1);
+        size--;
+      }
+      return true;
+    }
+    LOGGER.info("no key find to rdel for kvstore-list()");
+    return false;
   }
 }
