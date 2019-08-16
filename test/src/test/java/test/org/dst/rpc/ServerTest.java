b48b84a756ee667993be35cb569af3704ee6d5a3
kairbon <45480357+kairbon@users.noreply.github.com>
2019-08-16 21:11:09 +0800

    Refine rpc test (#74)
    * Refine rpc test
    
    * fix
    
    * update
    
    * remove the fallacious import
    
    * reset TestUtils.java

---
+++ test/src/test/java/test/org/dst/rpc/ServerTest.java
@@ -0,0 +1,36 @@
+package test.org.dst.rpc;
+
+import org.dst.server.generated.StringProtocol;
+import org.dst.server.service.DstStringService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ServerTest {
+    @Test
+    public void testRpcServer() {
+
+        ProxyOnClient<DstStringService> setProxy = new ProxyOnClient(DstStringService.class);
+        DstStringService stringService = setProxy.openConnection();
+
+        // Test string put request
+        StringProtocol.StringPutRequest stringPutRequest =
+                StringProtocol.StringPutRequest.newBuilder()
+                        .setKey("k1")
+                        .setValue("v1")
+                        .build();
+
+        StringProtocol.StringPutResponse stringResponse = stringService.strPut(stringPutRequest);
+        Assertions.assertEquals("ok", stringResponse.getStatus());
+
+        // Test string get request
+        StringProtocol.StringGetRequest strGetRequest =
+                StringProtocol.StringGetRequest.newBuilder()
+                        .setKey("k1")
+                        .build();
+
+        StringProtocol.StringGetResponse stringGetRequest = stringService.strGet(strGetRequest);
+        Assertions.assertEquals("v1", stringGetRequest.getValue());
+
+        setProxy.closeConnection();
+    }
+}
