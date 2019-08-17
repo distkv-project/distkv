0d48ea248048505683a42ba51a728acd63c379ae
WangQing <kingchin1218@126.com>
2019-08-17 14:42:54 +0800

    WIP

--- client/org/dst/client/DefaultDstClient.java
+++ client/org/dst/client/DefaultDstClient.java
@@ -1,6 +1,29 @@
 package org.dst.client;
 
+
+import com.baidu.brpc.client.BrpcProxy;
+import com.baidu.brpc.client.RpcClient;
+import com.baidu.brpc.client.RpcClientOptions;
+import com.baidu.brpc.protocol.Options;
+import org.dst.server.service.DstStringService;
+
 public class DefaultDstClient implements DstClient {
+
+    private RpcClient rcpClient;
+
+    private DstStringService stringProxy;
+
+    public DefaultDstClient(String serverAddress) {
+        RpcClientOptions clientOptions = new RpcClientOptions();
+        clientOptions.setProtocolType(Options.ProtocolType.PROTOCOL_BAIDU_STD_VALUE);
+        clientOptions.setWriteTimeoutMillis(1000);
+        clientOptions.setReadTimeoutMillis(1000);
+        clientOptions.setMaxTotalConnections(1000);
+        clientOptions.setMinIdleConnections(10);
+        rcpClient = new RpcClient(serverAddress, clientOptions);
+
+        stringProxy = BrpcProxy.getProxy(rcpClient, DstStringService.class);
+    }
 
     @Override
     public boolean connect() {
@@ -19,6 +42,6 @@ public class DefaultDstClient implements DstClient {
 
     @Override
     public DstStringProxy str() {
-        return null;
+        return new DstStringProxy(stringProxy);
     }
 }
--- client/org/dst/client/DstStringProxy.java
+++ client/org/dst/client/DstStringProxy.java
@@ -1,9 +1,21 @@
 package org.dst.client;
 
 import org.dst.client.exception.DstException;
+import org.dst.server.service.DstStringService;
 
-public interface DstStringProxy {
-    void put(String key, String value);
+public class DstStringProxy {
 
-    String get(String key) throws DstException;
+    private DstStringService service;
+
+    public DstStringProxy(DstStringService service) {
+        this.service = service;
+    }
+
+    public void put(String key, String value) {
+        
+    }
+
+    public String get(String key) throws DstException {
+
+    }
 }
