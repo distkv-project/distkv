d9f1d15981b2a05245d8cade4d0151d6002be7c5
senyer <576085903@qq.com>
2019-08-18 16:29:51 +0800

    support dst-list

--- core/src/main/java/org/dst/core/operatorImpl/DstListImpl.java
+++ core/src/main/java/org/dst/core/operatorImpl/DstListImpl.java
@@ -1,7 +1,8 @@
 package org.dst.core.operatorImpl;
 
-import org.dst.core.exception.NotImplementException;
 import org.dst.core.operatorset.DstList;
+import org.dst.utils.Status;
+
 import java.util.HashMap;
 import java.util.List;
 
@@ -14,42 +15,78 @@ public class DstListImpl implements DstList {
   }
 
   @Override
-  public void put(String key, List<String> value) {
+  public Status put(String key, List<String> value) {
     listMap.put(key, value);
+    return Status.OK;
   }
 
   @Override
   public List<String> get(String key) {
+    if (!listMap.containsKey(key)) {
+      //TODO should return a status value
+      return null;
+    }
     return listMap.get(key);
   }
 
   @Override
-  public boolean del(String key) {
+  public Status del(String key) {
     if (!listMap.containsKey(key)) {
-      return false;
+      return Status.KEY_NOT_FOUND;
     }
-
     listMap.remove(key);
-    return true;
+    return Status.OK;
   }
 
   @Override
-  public boolean lput(String key, List<String> value) {
-    throw new NotImplementException("The method is not implemented");
+  public Status lput(String key, List<String> value) {
+    if (!listMap.containsKey(key)) {
+      return Status.KEY_NOT_FOUND;
+    }
+    listMap.get(key).addAll(0, value);
+    return Status.OK;
   }
 
   @Override
-  public boolean rput(String key, List<String> value) {
-    throw new NotImplementException("The method is not implemented");
+  public Status rput(String key, List<String> value) {
+    if (!listMap.containsKey(key)) {
+      return Status.KEY_NOT_FOUND;
+    }
+    listMap.get(key).addAll(value);
+    return Status.OK;
   }
 
   @Override
-  public boolean ldel(String key, int n) {
-    throw new NotImplementException("The method is not implemented");
+  public Status ldel(String key, int n) {
+    if (!listMap.containsKey(key)) {
+      return Status.KEY_NOT_FOUND;
+    }
+    List<String> original = listMap.get(key);
+    if (original.size() < n) {
+      original.clear();
+      return Status.OK;
+    }
+    for (int i = 0; i < n; i++) {
+      original.remove(0);
+    }
+    return Status.OK;
   }
 
   @Override
-  public boolean rdel(String key, int n) {
-    throw new NotImplementException("The method is not implemented");
+  public Status rdel(String key, int n) {
+    if (!listMap.containsKey(key)) {
+      return Status.KEY_NOT_FOUND;
+    }
+    List<String> original = listMap.get(key);
+    int size = original.size();
+    if (size <= n) {
+      original.clear();
+      return Status.OK;
+    }
+    for (int i = 0; i < n; i++) {
+      original.remove(size - 1);
+      size--;
+    }
+    return Status.OK;
   }
 }
