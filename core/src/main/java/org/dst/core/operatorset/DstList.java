1934e4ce32905430eb30197a69c8c0190cdf4aeb
senyer <37206562+senyer@users.noreply.github.com>
2019-08-18 22:45:12 +0800

    Support dst-list concept (#93)
    * support dst-list
    
    * fix test faliure
    
    * support list rpc implements
    
    * enhance test module,
    Reduce duplicate code

--- core/src/main/java/org/dst/core/operatorset/DstList.java
+++ core/src/main/java/org/dst/core/operatorset/DstList.java
@@ -1,6 +1,8 @@
 package org.dst.core.operatorset;
 
 import org.dst.core.exception.NotImplementException;
+import org.dst.utils.Status;
+
 import java.util.List;
 
 public interface DstList {
@@ -11,7 +13,7 @@ public interface DstList {
    * @param key   the key to store
    * @param value the list value to store
    */
-  public void put(String key, List<String> value);
+  Status put(String key, List<String> value);
 
   /**
    * This method will query a list value based on the key
@@ -19,7 +21,7 @@ public interface DstList {
    * @param key Obtain a list value based on the key
    * @return the list value
    */
-  public List<String> get(String key);
+  List<String> get(String key);
 
   /**
    * This method will delete a list value based on the key
@@ -27,18 +29,18 @@ public interface DstList {
    * @param key delete a key-value pair based on the key
    * @return true or false, indicates that the deletion succeeded or failed.
    */
-  public boolean del(String key);
+  Status del(String key);
 
   //insert value from the left of list
-  public boolean lput(String key, List<String> value) throws NotImplementException;
+  Status lput(String key, List<String> value) throws NotImplementException;
 
   //insert value from the right of list
-  public boolean rput(String key, List<String> value) throws NotImplementException;
+  Status rput(String key, List<String> value) throws NotImplementException;
 
   //delete n values from the left of list
-  public boolean ldel(String key, int n) throws NotImplementException;
+  Status ldel(String key, int n) throws NotImplementException;
 
   //delete n values from the right of list
-  public boolean rdel(String key, int n) throws NotImplementException;
+  Status rdel(String key, int n) throws NotImplementException;
 
 }
