af6e74ab26b0e954ea8bc3afe7c8968e54a39d67
WangQing <kingchin1218@126.com>
2019-08-18 14:22:23 +0800

    Fix

--- test/src/test/java/test/org/dst/client/StringProxyTest.java
+++ test/src/test/java/test/org/dst/client/StringProxyTest.java
@@ -6,29 +6,35 @@ import org.dst.client.DefaultDstClient;
 import org.dst.client.DstClient;
 import org.dst.client.exception.KeyNotFoundException;
 import org.junit.jupiter.api.Test;
+import test.org.dst.rpc.TestUtil;
 
 
 public class StringProxyTest {
 
-    private final static String serverAddress = "";
+    private final static String serverAddress = "list://127.0.0.1:8082";
+
 
     @Test
     public void testPutAndGet() {
+        TestUtil.startRpcServer();
         DstClient client = new DefaultDstClient(serverAddress);
         client.str().put("k1", "v1");
 
         Assert.assertEquals("v1", client.str().get("k1"));
+        TestUtil.stopRpcServer();
     }
 
     @Test
     public void testKeyNotFoundWhenGetting() {
+        TestUtil.startRpcServer();
         DstClient client = new DefaultDstClient(serverAddress);
 
         try {
             client.str().get("k1");
+            Assert.assertTrue("It shouldn't reach here.", false);
         } catch (KeyNotFoundException e) {
             Assert.assertTrue(true);
         }
-        Assert.assertTrue("It shouldn't reach here.", false);
+        TestUtil.stopRpcServer();
     }
 }
