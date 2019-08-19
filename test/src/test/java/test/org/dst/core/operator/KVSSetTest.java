c88e9b2117f81607f314473b378098d3579f1b1d
senyer <576085903@qq.com>
2019-08-19 13:47:54 +0800

    Adjust variable name and remove empty lines

--- test/src/test/java/test/org/dst/core/operator/KVSSetTest.java
+++ test/src/test/java/test/org/dst/core/operator/KVSSetTest.java
@@ -5,7 +5,6 @@ import org.dst.core.KVStoreImpl;
 import org.dst.exception.KeyNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,19 +13,19 @@ public class KVSSetTest {
 
     @Test
     public void testSet() {
-        KVStore ks = new KVStoreImpl();
+        KVStore store = new KVStoreImpl();
         Set<String> set = new HashSet<>();
         set.add("v1");
         set.add("v2");
         set.add("v3");
-        ks.set().put("k1", set);
-        Assertions.assertEquals(set, ks.set().get("k1"));
+        store.set().put("k1", set);
+        Assertions.assertEquals(set, store.set().get("k1"));
         try {
-            Assertions.assertTrue(ks.set().exists("k1", "v3"));
+            Assertions.assertTrue(store.set().exists("k1", "v3"));
         } catch (KeyNotFoundException e) {
             e.printStackTrace();
         }
-        Assertions.assertTrue(ks.set().del("k1"));
-        Assertions.assertNull(ks.set().get("k1"));
+        Assertions.assertTrue(store.set().del("k1"));
+        Assertions.assertNull(store.set().get("k1"));
     }
 }
