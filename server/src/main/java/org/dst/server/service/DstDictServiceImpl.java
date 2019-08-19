7aa87f7f8bcd4f80cb50f63f60b4aa2a4d46a4e2
kairbon <45480357+kairbon@users.noreply.github.com>
2019-08-19 10:33:08 +0800

    add the dict proto file and service (#89)
    * chenge the sapce
    
    * add try-whth-resources block
    
    * add the dictService
    
    * add dictRpcTest
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

---
+++ server/src/main/java/org/dst/server/service/DstDictServiceImpl.java
@@ -0,0 +1,51 @@
+package org.dst.server.service;
+
+import org.dst.core.KVStore;
+import org.dst.server.generated.DictProtocol;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DstDictServiceImpl implements DstDictService {
+
+  private KVStore store;
+
+  public DstDictServiceImpl(KVStore kvStore) {
+    store = kvStore;
+  }
+
+  @Override
+  public DictProtocol.DictPutResponse put(DictProtocol.DictPutRequest request) {
+    DictProtocol.DictPutResponse.Builder responseBuilder =
+            DictProtocol.DictPutResponse.newBuilder();
+    String result;
+    try {
+      Map<String,String> map = new HashMap<>();
+      DictProtocol.DstDict dstDict = request.getDict();
+      for (int i = 0;i < dstDict.getKeysCount();i++) {
+        map.put(dstDict.getKeys(i), dstDict.getValues(i));
+      }
+      store.dict().put(request.getKey(), map);
+      result = "ok";
+    } catch (Exception e) {
+      // TODO: Use DstException instead of Exception here.
+      result = e.getMessage();
+    }
+    responseBuilder.setStatus(result);
+    return responseBuilder.build();
+  }
+
+  @Override
+  public DictProtocol.DictGetResponse get(DictProtocol.DictGetRequest request) {
+    DictProtocol.DictGetResponse.Builder responseBuilder =
+            DictProtocol.DictGetResponse.newBuilder();
+    Map<String,String> values = store.dict().get(request.getKey());
+    DictProtocol.DstDict.Builder builder = DictProtocol.DstDict.newBuilder();
+    for (Map.Entry<String,String> entry : values.entrySet()) {
+      builder.addKeys(entry.getKey());
+      builder.addValues(entry.getValue());
+    }
+    responseBuilder.setDict(builder.build());
+    responseBuilder.setStatus("ok");
+    return responseBuilder.build();
+  }
+}
