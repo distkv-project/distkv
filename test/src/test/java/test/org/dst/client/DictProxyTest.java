f521ef3066d402eb30503519aeabef813b2e876c
kairbon <wangkai31415926@gmail.com>
2019-08-21 14:16:08 +0800

    0.5

--- test/src/test/java/test/org/dst/client/DictProxyTest.java
+++ test/src/test/java/test/org/dst/client/DictProxyTest.java
@@ -2,6 +2,7 @@ package test.org.dst.client;
 
 import org.dst.client.DefaultDstClient;
 import org.dst.client.DstClient;
+import org.dst.exception.DstException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import test.org.dst.rpc.TestUtil;
@@ -24,21 +25,68 @@ public class DictProxyTest {
         TestUtil.stopRpcServer();
     }
 
-    @Test void testDictOtherOperator() {
+    @Test
+    public void testDictPutItem() {
         TestUtil.startRpcServer();
         DstClient client = new DefaultDstClient(serverAddress);
         Map<String, String> dict = new HashMap<>();
         dict.put("k1","v1");
         client.dicts().put("m1",dict);
         client.dicts().putItem("m1","k2","v2");
-        String v2 = client.dicts().popItem("m1","k2");
-        Assert.assertEquals(v2,"v2");
-        Map<String, String> dict1 = client.dicts().get("m1");
-        Assert.assertEquals(dict,dict1);
-        client.dicts().put("m2",dict);
-        final Map<String, String> m2 = client.dicts().get("m2");
+        final Map<String, String> m2 = client.dicts().get("m1");
+        dict.put("k2", "v2");
         Assert.assertEquals(dict,m2);
-        client.dicts().del("m2");
         TestUtil.stopRpcServer();
+    }
+
+    @Test
+    public void testDictGetItemValue() {
+        TestUtil.startRpcServer();
+        DstClient client = new DefaultDstClient(serverAddress);
+        Map<String, String> dict = new HashMap<>();
+        dict.put("k1","v1");
+        client.dicts().put("m1",dict);
+        String s1 = client.dicts().getItemValue("m1", "k1");
+        Assert.assertEquals("v1",s1);
+        TestUtil.stopRpcServer();
+    }
+
+    @Test
+    public void testDictPopItem() {
+        TestUtil.startRpcServer();
+        DstClient client = new DefaultDstClient(serverAddress);
+        Map<String, String> dict = new HashMap<>();
+        dict.put("k1", "v1");
+        dict.put("k2", "v2");
+        client.dicts().put("m1",dict);
+        String s1 = client.dicts().popItem("m1", "k1");
+        Assert.assertEquals("v1",s1);
+        dict.remove("k1");
+        Assert.assertEquals(dict,client.dicts().get("m1"));
+        TestUtil.stopRpcServer();
+    }
+
+    @Test
+    public void testDictDel() {
+        TestUtil.startRpcServer();
+        DstClient client = new DefaultDstClient(serverAddress);
+        Map<String, String> dict = new HashMap<>();
+        dict.put("k1", "v1");
+        dict.put("k2", "v2");
+        client.dicts().put("m1",dict);
+        client.dicts().del("m1");
+        TestUtil.stopRpcServer();
+    }
+
+    @Test
+    public void testException() {
+        TestUtil.startRpcServer();
+        DstClient client = new DefaultDstClient(serverAddress);
+        try {
+            client.dicts().del("m1");
+            TestUtil.stopRpcServer();
+        }catch (DstException e) {
+            TestUtil.stopRpcServer();
+        }
     }
 }
