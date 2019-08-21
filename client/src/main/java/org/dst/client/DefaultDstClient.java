0760f4c8a9b73aed0768f384ed68f808ef15688c
kairbon <wangkai31415926@gmail.com>
2019-08-21 10:21:06 +0800

    0.1

--- client/src/main/java/org/dst/client/DefaultDstClient.java
+++ client/src/main/java/org/dst/client/DefaultDstClient.java
@@ -4,6 +4,8 @@ import com.baidu.brpc.client.BrpcProxy;
 import com.baidu.brpc.client.RpcClient;
 import com.baidu.brpc.client.RpcClientOptions;
 import com.baidu.brpc.protocol.Options;
+import org.dst.server.generated.DictProtocol;
+import org.dst.server.service.DstDictService;
 import org.dst.server.service.DstStringService;
 
 public class DefaultDstClient implements DstClient {
@@ -11,6 +13,8 @@ public class DefaultDstClient implements DstClient {
   private RpcClient rcpClient;
 
   private DstStringService stringProxy;
+
+  private DstDictService dictProxy;
 
   public DefaultDstClient(String serverAddress) {
     RpcClientOptions clientOptions = new RpcClientOptions();
@@ -22,6 +26,8 @@ public class DefaultDstClient implements DstClient {
     rcpClient = new RpcClient(serverAddress, clientOptions);
 
     stringProxy = BrpcProxy.getProxy(rcpClient, DstStringService.class);
+    dictProxy = BrpcProxy.getProxy(rcpClient, DstDictService.class);
+
   }
 
   @Override
@@ -42,5 +48,10 @@ public class DefaultDstClient implements DstClient {
   @Override
   public DstStringProxy strs() {
     return new DstStringProxy(stringProxy);
+  }
+
+  @Override
+  public DstDictProxy dicts() {
+    return new DstDictProxy(dictProxy);
   }
 }
---
+++ client/src/main/java/org/dst/client/DstDictProxy.java
@@ -0,0 +1,112 @@
+package org.dst.client;
+
+import org.dst.core.operatorset.DstDict;
+import org.dst.exception.DstException;
+import org.dst.server.generated.CommonProtocol;
+import org.dst.server.generated.DictProtocol;
+import org.dst.server.service.DstDictService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DstDictProxy {
+
+    private DstDictService service;
+
+    public DstDictProxy(DstDictService service) {
+        this.service = service;
+    }
+
+    public void put(String key, Map<String, String> dict) {
+        DictProtocol.PutRequest.Builder request =
+                DictProtocol.PutRequest.newBuilder();
+        request.setKey(key);
+        DictProtocol.DstDict.Builder builder =
+                DictProtocol.DstDict.newBuilder();
+        for(Map.Entry<String, String> kv : dict.entrySet()) {
+            builder.addKeys(kv.getKey());
+            builder.addValues(kv.getValue());
+        }
+        request.setDict(builder.build());
+        DictProtocol.PutResponse response = service.put(request.build());
+        if(response.getStatus() != CommonProtocol.Status.OK) {
+            throw new DstException(String.format("Error code is %d", response.getStatus().getNumber()));
+        }
+    }
+
+    public Map<String, String> get(String key) {
+        Map<String,String> dict = new HashMap();
+        DictProtocol.GetRequest.Builder request =
+                DictProtocol.GetRequest.newBuilder();
+        request.setKey(key);
+        DictProtocol.GetResponse response =
+                service.get(request.build());
+        if(response.getStatus() != CommonProtocol.Status.OK) {
+            throw new DstException(String.format("Error code is %d", response.getStatus().getNumber()));
+        }
+        DictProtocol.DstDict dstDict = response.getDict();
+        for (int i = 0; i < dstDict.getKeysCount(); i++) {
+            dict.put(dstDict.getKeys(i), dstDict.getValues(i));
+        }
+        return dict;
+    }
+
+    public String getItemValue(String key, String itemKey) {
+        DictProtocol.GetItemValueRequest.Builder request =
+                DictProtocol.GetItemValueRequest.newBuilder();
+        request.setKey(key);
+        request.setItemKey(itemKey);
+        DictProtocol.GetItemValueResponse response =
+                service.getItemValue(request.build());
+        if(response.getStatus() != CommonProtocol.Status.OK) {
+            throw new DstException(String.format("Error code is %d", response.getStatus().getNumber()));
+        }
+        return response.getItemValue();
+    }
+
+    public String popItem(String key, String itemKey) {
+        DictProtocol.PopItemRequest.Builder request =
+                DictProtocol.PopItemRequest.newBuilder();
+        request.setKey(key);
+        request.setItemKey(itemKey);
+        DictProtocol.PopItemResponse response =
+                service.popItem(request.build());
+        if(response.getStatus() != CommonProtocol.Status.OK) {
+            throw new DstException(String.format("Error code is %d", response.getStatus().getNumber()));
+        }
+        return response.getItemValue();
+    }
+
+    public void putItem(String key, String itemKey, String itemValue) {
+        DictProtocol.PutItemRequest.Builder request =
+                DictProtocol.PutItemRequest.newBuilder();
+        request.setKey(key);
+        request.setItemKey(itemKey);
+        request.setItemValue(itemValue);
+        DictProtocol.PutItemResponse response = service.putItem(request.build());
+        if(response.getStatus() != CommonProtocol.Status.OK) {
+            throw new DstException(String.format("Error code is %d", response.getStatus().getNumber()));
+        }
+    }
+
+    public void del(String key) {
+        DictProtocol.DelRequest.Builder request =
+                DictProtocol.DelRequest.newBuilder();
+        request.setKey(key);
+        DictProtocol.DelResponse response = service.del(request.build());
+        if(response.getStatus() != CommonProtocol.Status.OK) {
+            throw new DstException(String.format("Error code is %d", response.getStatus().getNumber()));
+        }
+    }
+
+    public void delItem(String key, String itemKey) {
+        DictProtocol.DelItemRequest.Builder request =
+                DictProtocol.DelItemRequest.newBuilder();
+        request.setKey(key);
+        request.setItemKey(itemKey);
+        DictProtocol.DelItemResponse response = service.delItem(request.build());
+        if(response.getStatus() != CommonProtocol.Status.OK) {
+            throw new DstException(String.format("Error code is %d", response.getStatus().getNumber()));
+        }
+    }
+}
