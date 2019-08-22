e88ae1a1feeb72b7ed78cd4ae1c6ae4d270ed567
senyer <576085903@qq.com>
2019-08-22 17:20:10 +0800

    Merge branch 'master' of github.com:dst-project/dst into re_test_rpc_comm
    # Conflicts:
    #	test/src/test/java/test/org/dst/rpc/ListRpcTest.java
    #	test/src/test/java/test/org/dst/rpc/SetRpcTest.java
    #	test/src/test/java/test/org/dst/rpc/StringRpcTest.java

--- test/src/test/java/test/org/dst/server/service/SetRpcTest.java
+++ test/src/test/java/test/org/dst/server/service/SetRpcTest.java
@@ -10,7 +10,7 @@ import test.org.dst.supplier.BaseTestSupplier;
 import test.org.dst.supplier.ProxyOnClient;
 import java.util.List;
 
-public class SetRpcTest extends BaseTestSupplier{
+public class SetRpcTest extends BaseTestSupplier {
 
   @Test
   public void testSet() {
@@ -24,29 +24,29 @@ public class SetRpcTest extends BaseTestSupplier{
   }
 
   private static void testPut() {
-    try(ProxyOnClient<DstSetService> setProxy = new ProxyOnClient<>(DstSetService.class)) {
+    try (ProxyOnClient<DstSetService> setProxy = new ProxyOnClient<>(DstSetService.class)) {
       DstSetService setService = setProxy.getService();
       SetProtocol.PutRequest.Builder setPutRequestBuilder =
-            SetProtocol.PutRequest.newBuilder();
+              SetProtocol.PutRequest.newBuilder();
       setPutRequestBuilder.setKey("k1");
       final List<String> values = ImmutableList.of("v1", "v2", "v3", "v1");
       values.forEach(value -> setPutRequestBuilder.addValues(value));
 
       SetProtocol.PutResponse setPutResponse =
-            setService.put(setPutRequestBuilder.build());
+              setService.put(setPutRequestBuilder.build());
       Assert.assertEquals(CommonProtocol.Status.OK, setPutResponse.getStatus());
     }
   }
 
   private static void testGet() {
-    try(ProxyOnClient<DstSetService> setProxy = new ProxyOnClient<>(DstSetService.class)) {
+    try (ProxyOnClient<DstSetService> setProxy = new ProxyOnClient<>(DstSetService.class)) {
       DstSetService setService = setProxy.getService();
       SetProtocol.GetRequest.Builder setGetRequestBuilder =
-            SetProtocol.GetRequest.newBuilder();
+              SetProtocol.GetRequest.newBuilder();
       setGetRequestBuilder.setKey("k1");
 
       SetProtocol.GetResponse setGetResponse =
-            setService.get(setGetRequestBuilder.build());
+              setService.get(setGetRequestBuilder.build());
 
       final List<String> results = ImmutableList.of("v1", "v2", "v3");
       Assert.assertEquals(CommonProtocol.Status.OK, setGetResponse.getStatus());
@@ -57,15 +57,15 @@ public class SetRpcTest extends BaseTestSupplier{
 
 
   private static void testDelete() {
-    try(ProxyOnClient<DstSetService> setProxy = new ProxyOnClient<>(DstSetService.class)) {
+    try (ProxyOnClient<DstSetService> setProxy = new ProxyOnClient<>(DstSetService.class)) {
       DstSetService setService = setProxy.getService();
       SetProtocol.DeleteRequest.Builder setDeleteRequestBuilder =
-            SetProtocol.DeleteRequest.newBuilder();
+              SetProtocol.DeleteRequest.newBuilder();
       setDeleteRequestBuilder.setKey("k1");
       setDeleteRequestBuilder.setEntity("v1");
 
       SetProtocol.DeleteResponse setDeleteResponse =
-            setService.delete(setDeleteRequestBuilder.build());
+              setService.delete(setDeleteRequestBuilder.build());
 
       Assert.assertEquals(CommonProtocol.Status.OK, setDeleteResponse.getStatus());
     }
@@ -73,30 +73,30 @@ public class SetRpcTest extends BaseTestSupplier{
 
 
   private static void testDropByKey() {
-    try(ProxyOnClient<DstSetService> setProxy = new ProxyOnClient<>(DstSetService.class)) {
+    try (ProxyOnClient<DstSetService> setProxy = new ProxyOnClient<>(DstSetService.class)) {
       DstSetService setService = setProxy.getService();
 
       SetProtocol.DropByKeyRequest.Builder setDropByKeyRequestBuilder =
-            SetProtocol.DropByKeyRequest.newBuilder();
+              SetProtocol.DropByKeyRequest.newBuilder();
       setDropByKeyRequestBuilder.setKey("k1");
 
       SetProtocol.DropByKeyResponse setDropByKeyResponse =
-            setService.dropByKey(setDropByKeyRequestBuilder.build());
+              setService.dropByKey(setDropByKeyRequestBuilder.build());
 
       Assert.assertEquals(CommonProtocol.Status.OK, setDropByKeyResponse.getStatus());
     }
   }
 
   private static void testExists() {
-    try(ProxyOnClient<DstSetService> setProxy = new ProxyOnClient<>(DstSetService.class)) {
+    try (ProxyOnClient<DstSetService> setProxy = new ProxyOnClient<>(DstSetService.class)) {
       DstSetService setService = setProxy.getService();
       SetProtocol.ExistsRequest.Builder setExistRequestBuilder =
-            SetProtocol.ExistsRequest.newBuilder();
+              SetProtocol.ExistsRequest.newBuilder();
       setExistRequestBuilder.setKey("k1");
       setExistRequestBuilder.setEntity("v1");
 
       SetProtocol.ExistsResponse setExistResponse =
-            setService.exists(setExistRequestBuilder.build());
+              setService.exists(setExistRequestBuilder.build());
       Assert.assertEquals(CommonProtocol.Status.KEY_NOT_FOUND, setExistResponse.getStatus());
     }
   }
