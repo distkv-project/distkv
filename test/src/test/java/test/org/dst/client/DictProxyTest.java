b8d41141d0ea5fc334198b37ffa23396c6b13d25
kairbon <wangkai31415926@gmail.com>
2019-08-23 09:52:10 +0800

    0.8

--- test/src/test/java/test/org/dst/client/DictProxyTest.java
+++ test/src/test/java/test/org/dst/client/DictProxyTest.java
@@ -11,70 +11,70 @@ import java.util.Map;
 
 public class DictProxyTest extends BaseTestSupplier {
 
-    private final static String serverAddress = "list://127.0.0.1:8082";
+  private final static String serverAddress = "list://127.0.0.1:8082";
 
-    @Test
-    public void testDictPutGet() {
-        DstClient client = new DefaultDstClient(serverAddress);
-        Map<String, String> dict = new HashMap<>();
-        dict.put("k1", "v1");
-        client.dicts().put("m1", dict);
-        Map<String, String> dict1 = client.dicts().get("m1");
-        Assert.assertEquals(dict, dict1);
-    }
+  @Test
+  public void testDictPutGet() {
+    DstClient client = new DefaultDstClient(serverAddress);
+    Map<String, String> dict = new HashMap<>();
+    dict.put("k1", "v1");
+    client.dicts().put("m1", dict);
+    Map<String, String> dict1 = client.dicts().get("m1");
+    Assert.assertEquals(dict, dict1);
+  }
 
-    @Test
-    public void testDictPutItem() {
-        DstClient client = new DefaultDstClient(serverAddress);
-        Map<String, String> dict = new HashMap<>();
-        dict.put("k1", "v1");
-        client.dicts().put("m1", dict);
-        client.dicts().putItem("m1", "k2", "v2");
-        final Map<String, String> m2 = client.dicts().get("m1");
-        dict.put("k2", "v2");
-        Assert.assertEquals(dict, m2);
-    }
+  @Test
+  public void testDictPutItem() {
+    DstClient client = new DefaultDstClient(serverAddress);
+    Map<String, String> dict = new HashMap<>();
+    dict.put("k1", "v1");
+    client.dicts().put("m1", dict);
+    client.dicts().putItem("m1", "k2", "v2");
+    final Map<String, String> m2 = client.dicts().get("m1");
+    dict.put("k2", "v2");
+    Assert.assertEquals(dict, m2);
+  }
 
-    @Test
-    public void testDictGetItemValue() {
-        DstClient client = new DefaultDstClient(serverAddress);
-        Map<String, String> dict = new HashMap<>();
-        dict.put("k1", "v1");
-        client.dicts().put("m1", dict);
-        String s1 = client.dicts().getItemValue("m1", "k1");
-        Assert.assertEquals("v1", s1);
-    }
+  @Test
+  public void testDictGetItemValue() {
+    DstClient client = new DefaultDstClient(serverAddress);
+    Map<String, String> dict = new HashMap<>();
+    dict.put("k1", "v1");
+    client.dicts().put("m1", dict);
+    String s1 = client.dicts().getItemValue("m1", "k1");
+    Assert.assertEquals("v1", s1);
+  }
 
-    @Test
-    public void testDictPopItem() {
-        DstClient client = new DefaultDstClient(serverAddress);
-        Map<String, String> dict = new HashMap<>();
-        dict.put("k1", "v1");
-        dict.put("k2", "v2");
-        client.dicts().put("m1", dict);
-        String s1 = client.dicts().popItem("m1", "k1");
-        Assert.assertEquals("v1", s1);
-        dict.remove("k1");
-        Assert.assertEquals(dict,client.dicts().get("m1"));
-    }
+  @Test
+  public void testDictPopItem() {
+    DstClient client = new DefaultDstClient(serverAddress);
+    Map<String, String> dict = new HashMap<>();
+    dict.put("k1", "v1");
+    dict.put("k2", "v2");
+    client.dicts().put("m1", dict);
+    String s1 = client.dicts().popItem("m1", "k1");
+    Assert.assertEquals("v1", s1);
+    dict.remove("k1");
+    Assert.assertEquals(dict, client.dicts().get("m1"));
+  }
 
-    @Test
-    public void testDictDel() {
-        DstClient client = new DefaultDstClient(serverAddress);
-        Map<String, String> dict = new HashMap<>();
-        dict.put("k1", "v1");
-        dict.put("k2", "v2");
-        client.dicts().put("m1", dict);
-        client.dicts().del("m1");
-    }
+  @Test
+  public void testDictDel() {
+    DstClient client = new DefaultDstClient(serverAddress);
+    Map<String, String> dict = new HashMap<>();
+    dict.put("k1", "v1");
+    dict.put("k2", "v2");
+    client.dicts().put("m1", dict);
+    client.dicts().del("m1");
+  }
 
-    @Test
-    public void testException() {
-        DstClient client = new DefaultDstClient(serverAddress);
-        try {
-            client.dicts().del("m1");
-        }catch (DstException e) {
-            e.printStackTrace();
-        }
+  @Test
+  public void testException() {
+    DstClient client = new DefaultDstClient(serverAddress);
+    try {
+      client.dicts().del("m1");
+    } catch (DstException e) {
+      e.printStackTrace();
     }
+  }
 }
