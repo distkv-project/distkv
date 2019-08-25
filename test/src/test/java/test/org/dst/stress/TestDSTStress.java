91ca29b0f8f9045e4c8d945a1ff3e50b3a44324d
kairbon <wangkai31415926@gmail.com>
2019-08-25 19:54:43 +0800

    test

--- test/src/test/java/test/org/dst/stress/TestDSTStress.java
+++ test/src/test/java/test/org/dst/stress/TestDSTStress.java
@@ -3,9 +3,12 @@ package test.org.dst.stress;
 import org.dst.client.DefaultDstClient;
 import org.dst.client.DstClient;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+import test.org.dst.supplier.TestUtil;
 
-public class TestStringStress {
+import java.lang.reflect.Method;
+
+public class TestDSTStress {
 
   private static final String serverAddress = "list://127.0.0.1:8082";
 
@@ -15,12 +18,22 @@ public class TestStringStress {
     int max = 100000;
     long start = System.currentTimeMillis();
     for (long i = 0;i< max ; i++) {
-      client.strs().put(start+"Red!isTtest"+i,"testV"+(i-1));
+      client.strs().put(start+"DST"+i,"testV"+(i-1));
     }
-    String as = client.strs().get(start+"Red!isTtest"+50000);
+    String as = client.strs().get(start+"DST"+50000);
     long end = System.currentTimeMillis();
     System.out.println(end-start);
     Assert.assertEquals(as,"testV49999");
+  }
+
+  @BeforeClass
+  public void setupBase() {
+    TestUtil.startRpcServer();
+  }
+
+  @AfterClass
+  public void teardownBase() {
+    TestUtil.stopRpcServer();
   }
 
 }
--- test/src/test/java/test/org/dst/stress/TestRDSStress.java
+++ test/src/test/java/test/org/dst/stress/TestRDSStress.java
@@ -1,4 +1,25 @@
 package test.org.dst.stress;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import redis.clients.jedis.Jedis;
+
+
 public class TestRDSStress {
+
+  @Test(threadPoolSize = 5, invocationCount = 10)
+  public void testStringPut() {
+    Jedis jedis = new Jedis("127.0.0.1",6379);
+    int max = 100000;
+    long start = System.currentTimeMillis();
+    for (long i = 0;i< max ; i++) {
+      jedis.set(start+"RDS"+i,"testV"+(i-1));
+    }
+    String as = jedis.get(start+"RDS"+50000);
+    long end = System.currentTimeMillis();
+    System.out.println(end-start);
+    Assert.assertEquals(as,"testV49999");
+    jedis.close();
+  }
+
 }
