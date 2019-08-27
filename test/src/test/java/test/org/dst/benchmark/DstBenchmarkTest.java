3e33d2ea0eb879049c2c977b01f2a9e32c68c103
kairbon <wangkai31415926@gmail.com>
2019-08-27 16:01:12 +0800

    fix

---
+++ test/src/test/java/test/org/dst/benchmark/DstBenchmarkTest.java
@@ -0,0 +1,39 @@
+package test.org.dst.benchmark;
+
+import org.dst.client.DefaultDstClient;
+import org.dst.client.DstClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import test.org.dst.benchmark.core.DSTBenchmark;
+
+public class DstBenchmarkTest implements Runnable{
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(DSTBenchmark.class);
+
+  public DstClient client ;
+
+  public DstBenchmarkTest(DstClient client) {
+    this.client = client;
+  }
+
+  public void strPutStressTest() {
+    Thread thread = Thread.currentThread();
+    long id = thread.getId();
+    String name = Thread.currentThread().getName();
+    long start = System.currentTimeMillis();
+    for (int i = 0; i < 100000; i++) {
+      client.strs().put(name + i, "test" + i);
+    }
+    String as = client.strs().get(name + 59999);
+    long end = System.currentTimeMillis();
+    String str = "This test is Str put test, and this is thread-" + id + " and waste time =" + (end - start) + "; the result is " + "test59999".equals(as);
+    LOGGER.debug(str);
+    System.out.println(str);
+    client.disconnect();
+  }
+
+  @Override
+  public void run() {
+    strPutStressTest();
+  }
+}
---
+++ test/src/test/java/test/org/dst/benchmark/core/DSTBenchmark.java
@@ -0,0 +1,77 @@
+package test.org.dst.benchmark.core;
+
+import org.dst.client.DefaultDstClient;
+import org.dst.client.DstClient;
+import test.org.dst.benchmark.DstBenchmarkTest;
+import test.org.dst.supplier.TestUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DSTBenchmark {
+
+  public static final String serverAddress = "list://127.0.0.1:8082";
+
+  private int threadNum = 1;
+
+  private Runnable myTest = null;
+
+  private long timeOut = 100 * 1000;
+
+  public void setTestModule(Runnable myTest) {
+    this.myTest = myTest;
+  }
+
+  public void setThreadNum(int threadNum) {
+    this.threadNum = threadNum;
+  }
+
+  public void setTimeOut(long timeOut) {
+    this.timeOut = timeOut;
+  }
+
+  public void DstRun() {
+    try {
+      TestUtil.startRpcServer();
+      Thread.sleep(1000*10);
+      List<DstClient> clients = new ArrayList<>();
+      for (int i = 0; i < threadNum; i++) {
+        clients.add(new DefaultDstClient(serverAddress));
+      }
+      for (int i = 0; i < clients.size(); i++) {
+        new Thread(new DstBenchmarkTest(clients.get(i))).start();
+      }
+      Thread.sleep(timeOut);
+      TestUtil.stopRpcServer();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void run() {
+    try {
+      TestUtil.startRpcServer();
+      if (myTest == null) {
+        System.out.println("You must set test module");
+      } else {
+        for (int i = 0; i < threadNum; i++) {
+          new Thread(myTest).start();
+          Thread.sleep(10000);
+        }
+      }
+      Thread.sleep(timeOut);
+      TestUtil.stopRpcServer();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) {
+//    DSTBenchmark benchmark = new DSTBenchmark();
+//    benchmark.setTimeOut(100 * 1000);
+//    benchmark.setThreadNum(10);
+//    benchmark.DstRun();
+    TestUtil.startRpcServer();
+  }
+
+}
