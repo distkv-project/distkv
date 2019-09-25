eac3d63b28aa34322c2cdd9d70d4b3fc2e577dac
jinyuxin <15855528782@163.com>
2019-09-25 15:47:45 +0800

    delete ArgsParseUtil class

--- client/src/main/java/org/dst/client/cmd/DstCmdStarter.java
+++ client/src/main/java/org/dst/client/cmd/DstCmdStarter.java
@@ -3,10 +3,26 @@ package org.dst.client.cmd;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.function.Function;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import org.dst.client.DefaultDstClient;
-import org.dst.common.exception.DstException;
 
 public class DstCmdStarter {
+
+  private static final String DEFAULT_VERSION = "0.1.0";
+
+  @Parameter(names = {"--help", "-help"}, description = "Show help messages.",
+          help = true, order = -1)
+  private static boolean HELP = false;
+
+  @Parameter(names = {"--address"}, description = "Specify the address of server to connect.",
+          order = 1)
+  private static String ADDRESS = "127.0.0.1:8082";
+
+  @Parameter(names = {"-v", "-version", "-V"}, description = "Show the version of Dst.",
+          help = true, order = 2)
+  private static boolean VERSION = false;
 
   private static DefaultDstClient client;
 
@@ -15,16 +31,36 @@ public class DstCmdStarter {
 
   public static void main(String[] args) {
 
-    String address;
+    StringBuilder sb = new StringBuilder();
+    DstCmdStarter dstCmdStarter = new DstCmdStarter();
+    JCommander jcommander = JCommander.newBuilder().addObject(dstCmdStarter).build();
+
     try {
-      address = ArgsParseUtil.covertArgsToAddress(args);
-    } catch (DstException e) {
-      System.out.println(e.getMessage());
+      jcommander.parse(args);
+    } catch (ParameterException e) {
+      jcommander.setProgramName("dst");
+      jcommander.usage(sb);
+      System.out.println(sb.toString());
       return;
     }
 
+    if (HELP) {
+      jcommander.setProgramName("dst");
+      jcommander.usage(sb);
+      System.out.println(sb.toString());
+      return;
+    }
+
+    if (VERSION) {
+      System.out.println(DEFAULT_VERSION);
+      return;
+    }
+
+    sb.append("list://");
+    sb.append(ADDRESS);
+
     try {
-      client = new DefaultDstClient(address);
+      client = new DefaultDstClient(sb.toString());
     } catch (Exception e) {
       System.out.println("connect failure，please check your input.");
       return;
