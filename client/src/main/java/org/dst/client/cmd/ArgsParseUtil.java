04f54752d645998cfcdeb6e22ee0d8f9484c7a8a
jinyuxin <15855528782@163.com>
2019-09-25 11:08:45 +0800

    add jcommander plugin

--- client/src/main/java/org/dst/client/cmd/ArgsParseUtil.java
+++ client/src/main/java/org/dst/client/cmd/ArgsParseUtil.java
@@ -1,48 +1,56 @@
 package org.dst.client.cmd;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import org.dst.common.exception.DstException;
 
 public class ArgsParseUtil {
-  private static final String defaultHost = "127.0.0.1";
-  private static final String defaultPort = "8082";
+
+  private static final String DEFAULT_VERSION = "0.1.0";
+
+  @Parameter(names = {"--help", "-help"}, description = "show the help command",
+          help = true, order = 0)
+  private static boolean HELP = false;
+  @Parameter(names = {"-h", "-H"}, description = "specify the host of server to connect", order = 1)
+  private static String HOST = "127.0.0.1";
+  @Parameter(names = {"-p", "-P"}, description = "specify the port of server to connect", order = 2)
+  private static String PORT = "8082";
+  @Parameter(names = {"-v", "-version", "-V"}, description = "show the version of Dst",
+      help = true, order = 3)
+  private static boolean VERSION = false;
 
   /**
    * @param args the args like [-h,127.0.0.1,-p,8082] or [-h,127.0.0.1] or [-p,8082]
    * @return list://127.0.0.1:8082
    */
   public static String covertArgsToAddress(String[] args) throws DstException {
-    String address;
-    if (args.length == 2) {
-      //-p 8082 or -h 127.0.0.1
-      if ("-h".equals(args[0])) {
-        address = covertArgsToAddress(args[1], defaultPort);
-      } else if ("-p".equals(args[0])) {
-        address = covertArgsToAddress(defaultHost, args[1]);
-      } else {
-        throw new DstException("the args are wrong. the example input is \"-h 127.0.0.1 -p 8082\"");
-      }
-    } else if (args.length == 4) {
-      //-h 127.0.0.1 -p 8082 or -p 8082 -h 127.0.0.1
-      if ("-h".equals(args[0]) && "-p".equals(args[2])) {
-        address = covertArgsToAddress(args[1], args[3]);
-      } else if ("-p".equals((args[0])) && "-h".equals((args[2]))) {
-        address = covertArgsToAddress(args[3], args[1]);
-      } else {
-        throw new DstException("the args are wrong. the example input is \"-h 127.0.0.1 -p 8082\"");
-      }
-    } else {
-      throw new DstException("the args are wrong. the example input is \"-h 127.0.0.1 -p 8082\"");
+
+    StringBuilder sb = new StringBuilder();
+
+    ArgsParseUtil argsParseUtil = new ArgsParseUtil();
+    JCommander jcommander = JCommander.newBuilder().addObject(argsParseUtil).build();
+    try {
+      jcommander.parse(args);
+    } catch (Exception e) {
+      jcommander.setProgramName("dst");
+      jcommander.usage(sb);
+      throw new DstException(sb.toString());
     }
 
-    return address;
-  }
+    if (HELP) {
+      jcommander.setProgramName("dst");
+      jcommander.usage(sb);
+      throw new DstException(sb.toString());
+    }
 
-  private static String covertArgsToAddress(String host, String port) {
-    StringBuilder sb = new StringBuilder();
+    if (VERSION) {
+      throw new DstException(DEFAULT_VERSION);
+    }
+
     sb.append("list://");
-    sb.append(host);
+    sb.append(HOST);
     sb.append(":");
-    sb.append(port);
+    sb.append(PORT);
     return sb.toString();
   }
 
