adacee9c6e21cdc124bf9d539bff2c708bfa0fc2
Samuel King <810916965@qq.com>
2019-09-25 17:45:41 +0800

    Fix port bug (#209)
    * fix port bug
    
    * make changes according to comments
    
    * add an args parse util
    
    * remove one line
    
    * add jcommander plugin
    
    * remove redundant dependency
    
    * make help don't display
    
    * make changes
    
    * make changes
    
    * make changes
    
    * make changes
    
    * make changes
    
    * delete ArgsParseUtil class
    
    * make changes
    
    * make changes
    
    * make changes

--- client/src/main/java/org/dst/client/cmd/DstCmdStarter.java
+++ client/src/main/java/org/dst/client/cmd/DstCmdStarter.java
@@ -3,11 +3,26 @@ package org.dst.client.cmd;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.function.Function;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import org.dst.client.DefaultDstClient;
 
 public class DstCmdStarter {
 
-  private static final String defaultAddress = "list://127.0.0.1:8082";
+  private static final String DEFAULT_VERSION = "0.1.0";
+
+  @Parameter(names = {"--help", "-help"}, description = "Show help messages.",
+          help = true, order = -1)
+  private static boolean HELP = false;
+
+  @Parameter(names = {"--address"}, description = "Specify the address of server to connect.",
+          order = 1)
+  private static String ADDRESS = "127.0.0.1:8082";
+
+  @Parameter(names = {"-v", "-version", "-V"}, description = "Show the version of Dst.",
+          help = true, order = 2)
+  private static boolean VERSION = false;
 
   private static DefaultDstClient client;
 
@@ -15,12 +30,35 @@ public class DstCmdStarter {
           commandHandlers = new HashMap<>();
 
   public static void main(String[] args) {
-    //TODO(jyx) Check the server is open or not
 
-    if (args.length == 0) {
-      client = new DefaultDstClient(defaultAddress);
+    DstCmdStarter dstCmdStarter = new DstCmdStarter();
+    JCommander jcommander = JCommander.newBuilder().addObject(dstCmdStarter).build();
+    jcommander.setProgramName("dst");
+
+    try {
+      jcommander.parse(args);
+    } catch (ParameterException e) {
+      jcommander.usage();
+      return;
     }
-    //TODO(jyx) deal with -h 127.0.0.1 -p 8082
+
+    if (HELP) {
+      jcommander.usage();
+      return;
+    }
+
+    if (VERSION) {
+      System.out.println(DEFAULT_VERSION);
+      return;
+    }
+
+    try {
+      client = new DefaultDstClient(String.format("list://%s", ADDRESS));
+    } catch (Exception e) {
+      System.out.println(String.format("Failed to connect to dst server, %s, "
+              + "please check your input.", ADDRESS));
+      return;
+    }
 
     //register different operation type handler
     commandHandlers.put(DstOperationType.STRING, new StringHandler(client));
