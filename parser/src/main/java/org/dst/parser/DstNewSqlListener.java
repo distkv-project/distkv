7cf6716dcb5932a3d6425449320d708cc0ed7de2
Qing Wang <kingchin1218@gmail.com>
2019-11-07 17:35:17 +0800

    Parse `str` and `list` concept (#242)
    * Add list parser
    
    * Add test
    
    * add str command
    
    * Fix

--- parser/src/main/java/org/dst/parser/DstNewSqlListener.java
+++ parser/src/main/java/org/dst/parser/DstNewSqlListener.java
@@ -5,7 +5,9 @@ import org.dst.parser.generated.DstNewSQLBaseListener;
 import org.dst.parser.generated.DstNewSQLParser;
 import org.dst.parser.po.DstParsedResult;
 import org.dst.parser.po.RequestTypeEnum;
+import org.dst.rpc.protobuf.generated.ListProtocol;
 import org.dst.rpc.protobuf.generated.SetProtocol;
+import org.dst.rpc.protobuf.generated.StringProtocol;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +19,96 @@ public class DstNewSqlListener extends DstNewSQLBaseListener {
 
   public DstParsedResult getParsedResult() {
     return parsedResult;
+  }
+
+  @Override
+  public void enterStrPut(DstNewSQLParser.StrPutContext ctx) {
+    Preconditions.checkState(parsedResult == null);
+    Preconditions.checkState(ctx.children.size() == 3);
+
+    StringProtocol.PutRequest.Builder builder = StringProtocol.PutRequest.newBuilder();
+    builder.setKey(ctx.children.get(1).getText());
+    builder.setValue(ctx.children.get(2).getText());
+    parsedResult = new DstParsedResult(RequestTypeEnum.STR_PUT, builder.build());
+  }
+
+  @Override
+  public void enterStrGet(DstNewSQLParser.StrGetContext ctx) {
+    Preconditions.checkState(parsedResult == null);
+    Preconditions.checkState(ctx.children.size() == 2);
+
+    StringProtocol.GetRequest.Builder builder = StringProtocol.GetRequest.newBuilder();
+    builder.setKey(ctx.children.get(1).getText());
+    parsedResult = new DstParsedResult(RequestTypeEnum.STR_GET, builder.build());
+  }
+
+  @Override
+  public void enterListPut(DstNewSQLParser.ListPutContext ctx) {
+    Preconditions.checkState(parsedResult == null);
+    Preconditions.checkState(ctx.children.size() == 3);
+
+    ListProtocol.PutRequest.Builder builder = ListProtocol.PutRequest.newBuilder();
+    builder.setKey(ctx.children.get(1).getText());
+    final int valueSize = ctx.children.get(2).getChildCount();
+    for (int i = 0; i < valueSize; ++i) {
+      builder.addValues(ctx.children.get(2).getChild(i).getText());
+    }
+    parsedResult = new DstParsedResult(RequestTypeEnum.LIST_PUT, builder.build());
+  }
+
+  @Override
+  public void enterListGet(DstNewSQLParser.ListGetContext ctx) {
+    Preconditions.checkState(parsedResult == null);
+    Preconditions.checkState(ctx.children.size() == 2);
+    ListProtocol.GetRequest.Builder builder = ListProtocol.GetRequest.newBuilder();
+    builder.setKey(ctx.children.get(1).getText());
+    parsedResult = new DstParsedResult(RequestTypeEnum.LIST_GET, builder.build());
+  }
+
+  @Override
+  public void enterListLput(DstNewSQLParser.ListLputContext ctx) {
+    Preconditions.checkState(parsedResult == null);
+    Preconditions.checkState(ctx.children.size() == 3);
+    ListProtocol.LPutRequest.Builder builder = ListProtocol.LPutRequest.newBuilder();
+    builder.setKey(ctx.children.get(1).getText());
+    final int valueSize = ctx.children.get(2).getChildCount();
+    for (int i = 0; i < valueSize; ++i) {
+      builder.addValues(ctx.children.get(2).getChild(i).getText());
+    }
+    parsedResult = new DstParsedResult(RequestTypeEnum.LIST_LPUT, builder.build());
+  }
+
+  @Override
+  public void enterListRput(DstNewSQLParser.ListRputContext ctx) {
+    Preconditions.checkState(parsedResult == null);
+    Preconditions.checkState(ctx.children.size() == 3);
+    ListProtocol.RPutRequest.Builder builder = ListProtocol.RPutRequest.newBuilder();
+    builder.setKey(ctx.children.get(1).getText());
+    final int valueSize = ctx.children.get(2).getChildCount();
+    for (int i = 0; i < valueSize; ++i) {
+      builder.addValues(ctx.children.get(2).getChild(i).getText());
+    }
+    parsedResult = new DstParsedResult(RequestTypeEnum.LIST_RPUT, builder.build());
+  }
+
+  @Override
+  public void enterListLdel(DstNewSQLParser.ListLdelContext ctx) {
+    Preconditions.checkState(parsedResult == null);
+    Preconditions.checkState(ctx.children.size() == 3);
+    ListProtocol.LDelRequest.Builder builder = ListProtocol.LDelRequest.newBuilder();
+    builder.setKey(ctx.children.get(1).getText());
+    builder.setValues(Integer.valueOf(ctx.children.get(2).getText()));
+    parsedResult = new DstParsedResult(RequestTypeEnum.LIST_LDEL, builder.build());
+  }
+
+  @Override
+  public void enterListRdel(DstNewSQLParser.ListRdelContext ctx) {
+    Preconditions.checkState(parsedResult == null);
+    Preconditions.checkState(ctx.children.size() == 3);
+    ListProtocol.RDelRequest.Builder builder = ListProtocol.RDelRequest.newBuilder();
+    builder.setKey(ctx.children.get(1).getText());
+    builder.setValues(Integer.valueOf(ctx.children.get(2).getText()));
+    parsedResult = new DstParsedResult(RequestTypeEnum.LIST_RDEL, builder.build());
   }
 
   @Override
---
+++ test/src/test/java/org/dst/test/parser/ParseStringCommandTest.java
@@ -0,0 +1,51 @@
+package org.dst.test.parser;
+
+import org.dst.common.exception.DstException;
+import org.dst.parser.DstParser;
+import org.dst.parser.po.DstParsedResult;
+import org.dst.parser.po.RequestTypeEnum;
+import org.dst.rpc.protobuf.generated.StringProtocol;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ParseStringCommandTest {
+
+  private static final DstParser dstParser = new DstParser();
+
+  @Test
+  public void testPut() {
+    final String command = "str.put k1 v1";
+    DstParsedResult result = dstParser.parse(command);
+    Assert.assertEquals(result.getRequestType(), RequestTypeEnum.STR_PUT);
+    StringProtocol.PutRequest request = (StringProtocol.PutRequest) result.getRequest();
+    Assert.assertEquals(request.getKey(), "k1");
+    Assert.assertEquals(request.getValue(), "v1");
+  }
+
+  @Test
+  public void testGet() {
+    final String command = "str.get k1";
+    DstParsedResult result = dstParser.parse(command);
+    Assert.assertEquals(result.getRequestType(), RequestTypeEnum.STR_GET);
+    StringProtocol.GetRequest request = (StringProtocol.GetRequest) result.getRequest();
+    Assert.assertEquals(request.getKey(), "k1");
+  }
+
+  @Test(expectedExceptions = DstException.class)
+  public void testInvalidPutCommand() {
+    final String command = "str.put k1 v1 v2";
+    dstParser.parse(command);
+  }
+
+  @Test(expectedExceptions = DstException.class)
+  public void testInvalidGetCommand() {
+    final String command = "str.get k1 v1";
+    dstParser.parse(command);
+  }
+
+  @Test(expectedExceptions = DstException.class)
+  public void testInvalidStrCommand() {
+    final String command = "str.get";
+    dstParser.parse(command);
+  }
+}
