8cd0f3e1a568678b0b653f749b768bf5399f5ba0
senyer <576085903@qq.com>
2019-11-08 23:12:38 +0800

    Add mutli thread test
    Add method test
    
    fix comment in interface

--- common/src/main/java/org/dst/common/queue/DstConcurrentQueue.java
+++ common/src/main/java/org/dst/common/queue/DstConcurrentQueue.java
@@ -4,10 +4,10 @@ public interface DstConcurrentQueue<E> {
 
   /**
    * Inserts the specified element into this queue if it is possible to do so
-   * immediately without violating capacity restrictions
+   * immediately without violating capacity restrictions.
    *
-   * @param e the element to add
-   * @return bool
+   * @param e The element to add
+   * @return Whether the element is inserted.
    */
   boolean add(E e);
 
@@ -15,15 +15,15 @@ public interface DstConcurrentQueue<E> {
    * Inserts the specified element into this queue if it is possible to do
    * so immediately without violating capacity restrictions.
    *
-   * @param e the element to add
-   * @return bool
+   * @param e The element to be added.
+   * @return Whether the element is inserted.
    */
   boolean offer(E e);
 
   /**
    * Retrieves and removes the head of this queue.
    *
-   * @return the head of this queue
+   * @return The head of this queue.
    */
   E remove();
 
@@ -31,28 +31,28 @@ public interface DstConcurrentQueue<E> {
    * Retrieves and removes the head of this queue,
    * or returns  null if this queue is empty.
    *
-   * @return the head of this queue, or  null if this queue is empty
+   * @return The head of this queue, or  null if this queue is empty.
    */
   E poll();
 
   /**
-   * Retrieves, but does not remove, the head of this queue,
+   * Retrieves, but does not remove, the head of this queue.
    *
-   * @return the head of this queue, or null if this queue is empty
+   * @return The head of this queue, or null if this queue is empty.
    */
   E peek();
 
   /**
    * Returns true if this queue contains no elements.
    *
-   * @return true if this queue contains no elements
+   * @return True if this queue contains no elements.
    */
   boolean isEmpty();
 
   /**
    * Returns the number of elements in this queue.
    *
-   * @return the number of elements in this queue
+   * @return The number of elements in this queue.
    */
   int size();
 
