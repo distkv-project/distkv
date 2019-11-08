76bb5e66e5a8473cf318a68dca251ebe85e8763d
senyer <576085903@qq.com>
2019-11-08 23:14:35 +0800

    fix

--- test/src/test/java/org/dst/test/common/queue/DefaultConcurrentQueueTest.java
+++ test/src/test/java/org/dst/test/common/queue/DefaultConcurrentQueueTest.java
@@ -1,11 +1,8 @@
 package org.dst.test.common.queue;
 
-
 import org.dst.common.queue.DefaultConcurrentQueue;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 
 public class DefaultConcurrentQueueTest {
@@ -17,7 +14,7 @@ public class DefaultConcurrentQueueTest {
   public void testAdd() throws InterruptedException {
     //multi thread test add()
     DefaultConcurrentQueue<Integer> queue = new DefaultConcurrentQueue<>();
-    CountDownLatch latch=new CountDownLatch(THREAD_COUNT);
+    CountDownLatch latch = new CountDownLatch(THREAD_COUNT);
     for (int i = 0; i < THREAD_COUNT; i++) {
       new Thread(() -> {
         for (int j = 0; j < DATA_COUNT; j++) {
@@ -28,14 +25,14 @@ public class DefaultConcurrentQueueTest {
     }
     latch.await();
     int totalCount = queue.size();
-    Assert.assertEquals(THREAD_COUNT*DATA_COUNT,totalCount);
+    Assert.assertEquals(THREAD_COUNT * DATA_COUNT, totalCount);
   }
 
   @Test
   public void testOffer() throws InterruptedException {
     //multi thread test offer()
     DefaultConcurrentQueue<Long> queue = new DefaultConcurrentQueue<>();
-    CountDownLatch latch=new CountDownLatch(THREAD_COUNT);
+    CountDownLatch latch = new CountDownLatch(THREAD_COUNT);
     for (int i = 0; i < THREAD_COUNT; i++) {
       new Thread(() -> {
         for (int j = 0; j < DATA_COUNT; j++) {
@@ -46,7 +43,7 @@ public class DefaultConcurrentQueueTest {
     }
     latch.await();
     int totalCount = queue.size();
-    Assert.assertEquals(THREAD_COUNT*DATA_COUNT,totalCount);
+    Assert.assertEquals(THREAD_COUNT * DATA_COUNT, totalCount);
   }
 
   @Test
@@ -55,9 +52,9 @@ public class DefaultConcurrentQueueTest {
     queue.add("a");
     queue.add("b");
     queue.add("c");
-    Assert.assertEquals(queue.poll(),"a");
+    Assert.assertEquals(queue.poll(), "a");
     //test remove head element
-    Assert.assertEquals(queue.size(),2);
+    Assert.assertEquals(queue.size(), 2);
   }
 
   @Test
@@ -66,8 +63,8 @@ public class DefaultConcurrentQueueTest {
     queue.add("a");
     queue.add("b");
     queue.add("c");
-    Assert.assertEquals(queue.peek(),"a");
-    Assert.assertEquals(queue.size(),3);
+    Assert.assertEquals(queue.peek(), "a");
+    Assert.assertEquals(queue.size(), 3);
   }
 
   @Test
@@ -76,15 +73,15 @@ public class DefaultConcurrentQueueTest {
     queue.add("a");
     queue.add("b");
     queue.add("c");
-    Assert.assertEquals(queue.remove(),"a");
+    Assert.assertEquals(queue.remove(), "a");
     //test remove head element
-    Assert.assertEquals(queue.size(),2);
+    Assert.assertEquals(queue.size(), 2);
   }
 
   @Test
   public void testIsEmpty() {
     DefaultConcurrentQueue<String> queue = new DefaultConcurrentQueue<>();
-    Assert.assertEquals(true,queue.isEmpty());
+    Assert.assertEquals(true, queue.isEmpty());
   }
 
 }
