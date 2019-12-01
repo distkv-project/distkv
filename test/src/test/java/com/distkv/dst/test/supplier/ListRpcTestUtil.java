949fd5a68254d4373beddea283f7e015fbe5a699
jovany-wang <kingchin1218@126.com>
2019-12-01 18:08:48 +0800

    Remove unused file

--- test/src/test/java/com/distkv/dst/test/supplier/ListRpcTestUtil.java
+++
@@ -1,9 +0,0 @@
-package com.distkv.dst.test.supplier;
-
-import com.distkv.dst.rpc.protobuf.generated.CommonProtocol;
-import com.distkv.dst.rpc.protobuf.generated.ListProtocol;
-
-public class ListRpcTestUtil {
-
-
-}
