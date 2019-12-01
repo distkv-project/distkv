babf3446de88c7e52f0d3d6b6aee025f9999786c
jovany-wang <kingchin1218@126.com>
2019-12-01 20:55:19 +0800

    Fix CI

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# This script installs the unresolved dependencies.
+
+set -e
+set -x
+
+# Install drpc.
+DRPC_REPOSITORY_URL=https://github.com/dst-project/drpc.git
+DRPC_COMMIT_ID=a020f7a6f438da6870fd15d3f15a7c3b83107a14
+
+git clone ${DRPC_REPOSITORY_URL} drpc_tmp
+pushd drpc_tmp
+mvn clean install -DskipTests
+popd
+
+echo "Drpc was installed successfully."
+
+echo "All dependencies were installed successfully."
