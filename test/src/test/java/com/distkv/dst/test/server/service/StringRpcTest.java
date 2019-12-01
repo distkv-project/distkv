34ea86548db9f4e065a295036de334a578190691
jovany-wang <kingchin1218@126.com>
2019-12-01 21:35:10 +0800

    Fix string rpc test

--- test/src/test/java/com/distkv/dst/test/server/service/StringRpcTest.java
+++ test/src/test/java/com/distkv/dst/test/server/service/StringRpcTest.java
@@ -1,5 +1,6 @@
 package com.distkv.dst.test.server.service;
 
+import com.distkv.dst.common.utils.Utils;
 import com.distkv.dst.rpc.protobuf.generated.CommonProtocol;
 import com.distkv.dst.rpc.protobuf.generated.StringProtocol;
 import com.distkv.dst.rpc.service.DstStringService;
@@ -7,7 +8,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import com.distkv.dst.test.supplier.BaseTestSupplier;
 import com.distkv.dst.test.supplier.ProxyOnClient;
-import java.util.concurrent.CompletableFuture;
 
 public class StringRpcTest extends BaseTestSupplier {
   @Test
@@ -15,19 +15,15 @@ public class StringRpcTest extends BaseTestSupplier {
     try (ProxyOnClient<DstStringService> stringProxy = new ProxyOnClient<>(
         DstStringService.class, rpcServerPort)) {
       DstStringService stringService = stringProxy.getService();
-      // Test string put request
+      // Test string put request.
       StringProtocol.PutRequest putRequest =
               StringProtocol.PutRequest.newBuilder()
                       .setKey("k1")
                       .setValue("v1")
                       .build();
 
-      CompletableFuture<StringProtocol.PutResponse> putFuture = stringService.put(putRequest);
-      // TODO(qwang): Use get instead of whenComplete.
-      putFuture.whenComplete((response, throwable) -> {
-        Assert.assertEquals(CommonProtocol.Status.OK, response.getStatus());
-        Assert.assertNotNull(throwable);
-      });
+      StringProtocol.PutResponse response = Utils.getFromFuture(stringService.put(putRequest));
+      Assert.assertEquals(CommonProtocol.Status.OK, response.getStatus());
 
       // Test string get request.
       StringProtocol.GetRequest getRequest =
@@ -35,12 +31,9 @@ public class StringRpcTest extends BaseTestSupplier {
                       .setKey("k1")
                       .build();
 
-      CompletableFuture<StringProtocol.GetResponse> getFuture = stringService.get(getRequest);
-      getFuture.whenComplete((response, throwable) -> {
-        Assert.assertEquals(CommonProtocol.Status.OK, response.getStatus());
-        Assert.assertEquals("v1", response.getValue());
-        Assert.assertNotNull(throwable);
-      });
+      StringProtocol.GetResponse response2 = Utils.getFromFuture(stringService.get(getRequest));
+      Assert.assertEquals(CommonProtocol.Status.OK, response2.getStatus());
+      Assert.assertEquals("v1", response2.getValue());
     }
   }
 }
