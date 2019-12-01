0fd17b6b984c5b3052f87cbebb937533c781d14a
senyer <576085903@qq.com>
2019-12-01 22:14:47 +0800

    [WIP] Add worker pool

---
+++ common/src/main/java/com/distkv/dst/common/worker/DstExecutor.java
@@ -0,0 +1,13 @@
+package com.distkv.dst.common.worker;
+
+public interface DstExecutor {
+
+  /**
+   * Executes the given task at some time in the future.  The command
+   * may execute in a new thread, in a pooled thread, or in the calling
+   * thread, at the discretion of the Executor implementation.
+   *
+   * @param runnable the task
+   */
+  void exec(Runnable runnable);
+}
---
+++ common/src/main/java/com/distkv/dst/common/worker/ThreadPool.java
@@ -0,0 +1,150 @@
+package com.distkv.dst.common.worker;
+
+import com.distkv.dst.common.work.DstExecutor;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public final class ThreadPool implements DstExecutor {
+
+    private volatile boolean shutdown;
+    private static final int DEFAULT_QUEUE_SIZE = 1024;
+
+    private ArrayDeque<Runnable> produced;
+    private ArrayDeque<Runnable> toConsume;
+    private ReentrantLock consumerLock;
+    private ReentrantLock producerLock;
+    private Condition isProducerNotEmptyCondition;
+    private ArrayList<Thread> threads;
+
+
+
+    public ThreadPool(int initialThreadNum) {
+        this(initialThreadNum, 0);
+    }
+
+    public ThreadPool(int initialThreadNum, int queueSize) {
+        if (initialThreadNum <= 0) {
+            throw new IllegalArgumentException(
+                  String.format("ThreadPool initialThreadNum %s should be positive ",initialThreadNum));
+        }
+        threads = new ArrayList<>(initialThreadNum);
+        shutdown = false;
+
+        if (queueSize <= 0) {
+            queueSize = DEFAULT_QUEUE_SIZE;
+        }
+        produced = new ArrayDeque<>(queueSize);
+        toConsume = new ArrayDeque<>(queueSize);
+        consumerLock = new ReentrantLock();
+        producerLock = new ReentrantLock();
+        isProducerNotEmptyCondition = producerLock.newCondition();
+        for (int i = 0; i < initialThreadNum; ++i) {
+            Thread thread = new Thread(() -> consume());
+            thread.start();
+            threads.add(thread);
+        }
+    }
+
+    @Override
+    public void exec(Runnable runnable) {
+
+    }
+
+    private void consume() {
+        while (true) {
+            Runnable task = null;
+            while (true) {
+                consumerLock.lock();
+                try {
+                    if (!toConsume.isEmpty()) {
+                        task = toConsume.pop();
+                        break;
+                    }
+                } finally {
+                    consumerLock.unlock();
+                }
+
+                producerLock.lock();
+                try {
+                    while (!shutdown && produced.isEmpty()) {
+                        try {
+                            isProducerNotEmptyCondition.await();
+                        } catch (InterruptedException ex) {
+                            // ignore
+                        }
+                    }
+                    if (!produced.isEmpty()) {
+                        consumerLock.lock();
+                        try {
+                            ArrayDeque<Runnable> tmp = produced;
+                            produced = toConsume;
+                            toConsume = tmp;
+                        } finally {
+                            consumerLock.unlock();
+                        }
+                    } else {
+                        // stopped must be true
+                        break;
+                    }
+                } finally {
+                    producerLock.unlock();
+                }
+            }
+            if (task != null) {
+                task.run();
+            } else {
+                // The thread pool was shut down
+                break;
+            }
+        }
+    }
+
+    public void shutdowm() {
+        shutdown = true;
+        producerLock.lock();
+        try {
+            isProducerNotEmptyCondition.signalAll();
+        } finally {
+            producerLock.unlock();
+        }
+    }
+
+    public boolean submit(Runnable task) {
+        Runnable[] tasks = { task };
+        return submit(tasks, 0, 1) == 1;
+    }
+
+    public long submit(Runnable []tasks, int offset, int len) {
+        int cur = offset;
+        int end = offset + len;
+        while (!shutdown && cur < end) {
+            producerLock.lock();
+            try {
+                int toProduce = end - cur;
+                if (toProduce > 0) {
+                    boolean wasEmpty = produced.isEmpty();
+                    int last = cur + toProduce;
+                    for (int i = offset; i < last; i++) {
+                        produced.addLast(tasks[i]);
+                    }
+                    if (wasEmpty) {
+                        isProducerNotEmptyCondition.signalAll();
+                    }
+                }
+                cur += toProduce;
+            } finally {
+                producerLock.unlock();
+            }
+        }
+        return cur - offset;
+    }
+
+    public boolean isShutdowm() {
+        return shutdown;
+    }
+
+}
+
