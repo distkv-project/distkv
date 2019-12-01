6f625c00aaa4ef038cf2fde1969ffeeace291d5e
jovany-wang <kingchin1218@126.com>
2019-12-01 23:22:02 +0800

    Add async service for dict.

--- common/src/main/java/com/distkv/dst/common/utils/FutureUtils.java
+++ common/src/main/java/com/distkv/dst/common/utils/FutureUtils.java
@@ -2,6 +2,7 @@ package com.distkv.dst.common.utils;
 
 import com.distkv.dst.common.exception.DstException;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -18,6 +19,15 @@ public class FutureUtils {
     } catch (ExecutionException e) {
       throw new DstException("Failed to get object from future.", e);
     }
+  }
+
+  /**
+   * Create a completable future object with the completed value.
+   */
+  public static <T> CompletableFuture<T> newCompletableFuture(T value) {
+    CompletableFuture<T> completableFuture= new CompletableFuture<>();
+    completableFuture.complete(value);
+    return completableFuture;
   }
 
 }
--- server/src/main/java/com/distkv/dst/server/service/DstDictServiceImpl.java
+++ server/src/main/java/com/distkv/dst/server/service/DstDictServiceImpl.java
@@ -1,5 +1,6 @@
 package com.distkv.dst.server.service;
 
+import com.distkv.dst.common.utils.FutureUtils;
 import com.distkv.dst.core.KVStore;
 import com.distkv.dst.rpc.protobuf.generated.CommonProtocol;
 import com.distkv.dst.rpc.protobuf.generated.DictProtocol;
@@ -9,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 public class DstDictServiceImpl extends DstBaseService implements DstDictService {
 
@@ -19,7 +21,7 @@ public class DstDictServiceImpl extends DstBaseService implements DstDictService
   }
 
   @Override
-  public DictProtocol.PutResponse put(DictProtocol.PutRequest request) {
+  public CompletableFuture<DictProtocol.PutResponse> put(DictProtocol.PutRequest request) {
     DictProtocol.PutResponse.Builder responseBuilder =
           DictProtocol.PutResponse.newBuilder();
     try {
@@ -31,22 +33,22 @@ public class DstDictServiceImpl extends DstBaseService implements DstDictService
       getStore().dicts().put(request.getKey(), map);
       responseBuilder.setStatus(CommonProtocol.Status.OK);
     } catch (Exception e) {
-      // TODO: Use DstException instead of Exception here.
+      // TODO(qwang): Use DstException instead of Exception here.
       responseBuilder.setStatus(CommonProtocol.Status.KEY_NOT_FOUND);
     }
 
-    return responseBuilder.build();
+    return FutureUtils.newCompletableFuture(responseBuilder.build());
   }
 
   @Override
-  public DictProtocol.GetResponse get(DictProtocol.GetRequest request) {
+  public CompletableFuture<DictProtocol.GetResponse> get(DictProtocol.GetRequest request) {
     DictProtocol.GetResponse.Builder responseBuilder =
           DictProtocol.GetResponse.newBuilder();
     responseBuilder.setStatus(CommonProtocol.Status.OK);
     final Map<String, String> dict = getStore().dicts().get(request.getKey());
     if (dict == null) {
       responseBuilder.setStatus(CommonProtocol.Status.KEY_NOT_FOUND);
-      return responseBuilder.build();
+      return FutureUtils.newCompletableFuture(responseBuilder.build());
     }
     DictProtocol.DstDict.Builder builder = DictProtocol.DstDict.newBuilder();
     for (Map.Entry<String, String> entry : dict.entrySet()) {
@@ -54,91 +56,91 @@ public class DstDictServiceImpl extends DstBaseService implements DstDictService
       builder.addValues(entry.getValue());
     }
     responseBuilder.setDict(builder.build());
-    return responseBuilder.build();
+    return FutureUtils.newCompletableFuture(responseBuilder.build());
   }
 
   @Override
-  public DictProtocol.GetItemResponse getItemValue(DictProtocol.GetItemRequest request) {
+  public CompletableFuture<DictProtocol.GetItemResponse> getItemValue(DictProtocol.GetItemRequest request) {
     DictProtocol.GetItemResponse.Builder responseBuilder =
           DictProtocol.GetItemResponse.newBuilder();
     final Map<String, String> dict = getStore().dicts().get(request.getKey());
     responseBuilder.setStatus(CommonProtocol.Status.OK);
     if (dict == null) {
       responseBuilder.setStatus(CommonProtocol.Status.KEY_NOT_FOUND);
-      return responseBuilder.build();
+      return FutureUtils.newCompletableFuture(responseBuilder.build());
     }
     final String itemValue = dict.get(request.getItemKey());
     if (itemValue == null) {
       responseBuilder.setStatus(CommonProtocol.Status.DICT_KEY_NOT_FOUND);
-      return responseBuilder.build();
+      return FutureUtils.newCompletableFuture(responseBuilder.build());
     }
     responseBuilder.setItemValue(itemValue);
-    return responseBuilder.build();
+    return FutureUtils.newCompletableFuture(responseBuilder.build());
   }
 
   @Override
-  public DictProtocol.PopItemResponse popItem(DictProtocol.PopItemRequest request) {
+  public CompletableFuture<DictProtocol.PopItemResponse> popItem(DictProtocol.PopItemRequest request) {
     DictProtocol.PopItemResponse.Builder responseBuilder =
           DictProtocol.PopItemResponse.newBuilder();
     responseBuilder.setStatus(CommonProtocol.Status.OK);
     final Map<String, String> dict = getStore().dicts().get(request.getKey());
     if (dict == null) {
       responseBuilder.setStatus(CommonProtocol.Status.KEY_NOT_FOUND);
-      return responseBuilder.build();
+      return FutureUtils.newCompletableFuture(responseBuilder.build());
     }
     final String itemValue = dict.remove(request.getItemKey());
     if (itemValue == null) {
       responseBuilder.setStatus(CommonProtocol.Status.DICT_KEY_NOT_FOUND);
-      return responseBuilder.build();
+      return FutureUtils.newCompletableFuture(responseBuilder.build());
     } else {
       responseBuilder.setItemValue(itemValue);
     }
-    return responseBuilder.build();
+    return FutureUtils.newCompletableFuture(responseBuilder.build());
   }
 
   @Override
-  public DictProtocol.PutItemResponse putItem(DictProtocol.PutItemRequest request) {
+  public CompletableFuture<DictProtocol.PutItemResponse> putItem(DictProtocol.PutItemRequest request) {
     DictProtocol.PutItemResponse.Builder responseBuilder =
           DictProtocol.PutItemResponse.newBuilder();
     responseBuilder.setStatus(CommonProtocol.Status.OK);
     final Map<String, String> dict = getStore().dicts().get(request.getKey());
     if (dict == null) {
       responseBuilder.setStatus(CommonProtocol.Status.KEY_NOT_FOUND);
-      return responseBuilder.build();
+      return FutureUtils.newCompletableFuture(responseBuilder.build());
     }
     dict.put(request.getItemKey(), request.getItemValue());
-    return responseBuilder.build();
+    return FutureUtils.newCompletableFuture(responseBuilder.build());
   }
 
   @Override
-  public DictProtocol.RemoveItemResponse removeItem(DictProtocol.RemoveItemRequest request) {
+  public CompletableFuture<DictProtocol.RemoveItemResponse> removeItem(DictProtocol.RemoveItemRequest request) {
     DictProtocol.RemoveItemResponse.Builder responseBuilder =
           DictProtocol.RemoveItemResponse.newBuilder();
     responseBuilder.setStatus(CommonProtocol.Status.OK);
     final Map<String, String> dict = getStore().dicts().get(request.getKey());
     if (dict == null) {
       responseBuilder.setStatus(CommonProtocol.Status.KEY_NOT_FOUND);
-      return responseBuilder.build();
+      return FutureUtils.newCompletableFuture(responseBuilder.build());
     }
     final String itemValue = dict.remove(request.getItemKey());
     if (itemValue == null) {
       responseBuilder.setStatus(CommonProtocol.Status.DICT_KEY_NOT_FOUND);
-      return responseBuilder.build();
+      return FutureUtils.newCompletableFuture(responseBuilder.build());
     }
     dict.remove(request.getItemKey());
-    return responseBuilder.build();
+    return FutureUtils.newCompletableFuture(responseBuilder.build());
   }
 
 
   @Override
-  public CommonProtocol.DropResponse drop(CommonProtocol.DropRequest request) {
+  public CompletableFuture<CommonProtocol.DropResponse> drop(CommonProtocol.DropRequest request) {
     CommonProtocol.DropResponse.Builder responseBuilder =
         CommonProtocol.DropResponse.newBuilder();
     responseBuilder.setStatus(CommonProtocol.Status.OK);
     if (!getStore().dicts().drop(request.getKey())) {
       responseBuilder.setStatus(CommonProtocol.Status.KEY_NOT_FOUND);
-      return responseBuilder.build();
+      return FutureUtils.newCompletableFuture(responseBuilder.build());
     }
-    return responseBuilder.build();
+    return FutureUtils.newCompletableFuture(responseBuilder.build());
   }
 }
