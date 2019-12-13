bf483413c1f981105bc96262a16a210913cfbdf8
kairbon <45480357+kairbon@users.noreply.github.com>
2019-12-13 22:14:47 +0800

    Add install_dependencies_win.bat (#336)
    * add addWinScript
    
    * fix
    
    * Rename install_dependencies.bat to install_dependencies_win.bat
    
    * fix

--- client/src/main/java/com/distkv/dst/client/commandlinetool/DstCommandExecutor.java
+++ client/src/main/java/com/distkv/dst/client/commandlinetool/DstCommandExecutor.java
@@ -13,8 +13,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.LinkedList;
+import java.util.Set;
 
 public class DstCommandExecutor {
 
