a979a41404ae1022e29e74b7aa0328acb7705658
WangQing <kingchin1218@126.com>
2019-12-18 01:18:53 +0800

    Fix CI

--- test/src/test/java/com/distkv/dst/test/core/operator/KVSDictTest.java
+++ test/src/test/java/com/distkv/dst/test/core/operator/KVSDictTest.java
@@ -1,5 +1,6 @@
 package com.distkv.dst.test.core.operator;
 
+import com.distkv.dst.common.exception.KeyNotFoundException;
 import com.distkv.dst.common.utils.Status;
 import com.distkv.dst.core.KVStore;
 import com.distkv.dst.core.KVStoreImpl;
@@ -9,7 +10,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class KVSDictTest {
-  @Test
+
+  @Test(expectedExceptions = KeyNotFoundException.class)
   public void testDict() {
     KVStore store = new KVStoreImpl();
     Map<String, String> dict = new HashMap<String, String>();
@@ -19,6 +21,6 @@ public class KVSDictTest {
     store.dicts().put("k1", dict);
     Assert.assertEquals(dict, store.dicts().get("k1"));
     Assert.assertEquals(Status.OK, store.dicts().drop("k1"));
-    Assert.assertNull(store.dicts().get("k1"));
+    store.dicts().get("k1");
   }
 }
--- test/src/test/java/com/distkv/dst/test/core/operator/KVSStrTest.java
+++ test/src/test/java/com/distkv/dst/test/core/operator/KVSStrTest.java
@@ -1,5 +1,6 @@
 package com.distkv.dst.test.core.operator;
 
+import com.distkv.dst.common.exception.KeyNotFoundException;
 import com.distkv.dst.common.utils.Status;
 import com.distkv.dst.core.KVStore;
 import com.distkv.dst.core.KVStoreImpl;
@@ -7,7 +8,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class KVSStrTest {
-  @Test
+
+  @Test(expectedExceptions = KeyNotFoundException.class)
   public void testStr() {
     KVStore store = new KVStoreImpl();
     store.strs().put("k1", "v1");
@@ -15,7 +17,7 @@ public class KVSStrTest {
     Assert.assertEquals("v1", store.strs().get("k1"));
     Assert.assertEquals("v2", store.strs().get("k2"));
     Assert.assertEquals(Status.OK, store.strs().drop("k1"));
-    Assert.assertNull(store.strs().get("k1"));
+    store.strs().get("k1");
   }
 
 }
