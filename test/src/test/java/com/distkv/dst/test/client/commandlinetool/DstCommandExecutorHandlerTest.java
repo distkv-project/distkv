694f53028f77959a956e52d4e8605bab50f54b8d
tortuo <tortuo369@gmail.com>
2019-12-18 20:15:39 +0800

    fix

--- test/src/test/java/com/distkv/dst/test/client/commandlinetool/DstCommandExecutorHandlerTest.java
+++ test/src/test/java/com/distkv/dst/test/client/commandlinetool/DstCommandExecutorHandlerTest.java
@@ -211,7 +211,7 @@ public class DstCommandExecutorHandlerTest extends BaseTestSupplier {
     command = "slist.getMember k1 m2";
     dstParsedResult = dstParser.parse(command);
     Assert.assertEquals(CommandExecutorHandler.
-            slistGetMember(dstClient, dstParsedResult), "22");
+            slistGetMember(dstClient, dstParsedResult), "(m2, 22), 1st");
 
     // Test command 'slist.drop'
     command = "slist.drop k1";
