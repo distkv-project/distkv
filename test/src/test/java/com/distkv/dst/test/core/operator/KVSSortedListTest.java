3f0793a0530956e9759760e20b558c6e279225e0
tortuo <tortuo369@gmail.com>
2019-12-27 00:15:03 +0800

    fix

--- test/src/test/java/com/distkv/dst/test/core/operator/KVSSortedListTest.java
+++ test/src/test/java/com/distkv/dst/test/core/operator/KVSSortedListTest.java
@@ -6,32 +6,25 @@ import com.distkv.dst.common.entity.sortedList.SortedListEntity;
 import com.distkv.dst.test.supplier.BaseTestSupplier;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import java.util.LinkedList;
 import java.util.List;
 
 public class KVSSortedListTest extends BaseTestSupplier {
   @Test
-  public void testSortedList() throws InterruptedException {
+  public void testSortedList() {
     KVStore store = new KVStoreImpl();
 
     LinkedList<SortedListEntity> list = new LinkedList<>();
     list.add(new SortedListEntity("xswl", 9));
     list.add(new SortedListEntity("wlll", 8));
-    list.add(new SortedListEntity("fw", 9));
+    list.add(new SortedListEntity("fw", 10));
     list.add(new SortedListEntity("55", 6));
     store.sortLists().put("k1", list);
     store.sortLists().putMember("k1", new SortedListEntity("asd",1000));
-    new Thread(() -> {
-      store.sortLists().incrScore("k1", "xswl",1);
-    }).start();
 
-    Runnable runnable = () -> {
-      store.sortLists().incrScore("k1", "fw",1);
-    };
-    new Thread(runnable).start();
-    new Thread(runnable).start();
-
-    Thread.sleep(100);
+    store.sortLists().incrScore("k1", "xswl",1);
+    store.sortLists().incrScore("k1", "fw",1);
     List<SortedListEntity> k11 = store.sortLists().top("k1", 3);
     Assert.assertEquals(k11.get(1).getMember(),"fw");
     Assert.assertEquals(k11.get(1).getScore(),11);
