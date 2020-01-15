f6e6a8a7d876c995a938c6e7adf58c7627044345
kairbon <wangkai31415926@gmail.com>
2020-01-15 22:19:55 +0800

    fix

--- test/src/test/java/com/distkv/dst/mastersalversync/TestMasterSalverSync.java
+++ test/src/test/java/com/distkv/dst/mastersalversync/TestMasterSalverSync.java
@@ -18,106 +18,75 @@ import java.util.concurrent.TimeUnit;
 public class TestMasterSalverSync {
 
   @Test
-  public void testStrPut() throws InterruptedException {
+  public void mainTest() throws InterruptedException {
     MasterSalverTestUtil.startAllProcess();
     TimeUnit.SECONDS.sleep(5);
-
-    DstClient client = new DefaultDstClient(String.format("list://127.0.0.1:%d", 8082));
-    client.strs().put("k1", "v1");
-    Assert.assertEquals("v1", client.strs().get("k1"));
+    DstClient client0 = new DefaultDstClient(String.format("list://127.0.0.1:%d", 8082));
     DstClient client1 = new DefaultDstClient(String.format("list://127.0.0.1:%d", 8090));
+
+    //test
+    testStrPut(client0, client1);
+    testListPut(client0, client1);
+    testSetPut(client0, client1);
+    testDictPut(client0, client1);
+    testSlistPut(client0, client1);
+
+    MasterSalverTestUtil.stopAllProcess();
+    TimeUnit.SECONDS.sleep(1);
+  }
+
+  public void testStrPut(DstClient client0, DstClient client1) {
+    client0.strs().put("k1", "v1");
+    Assert.assertEquals("v1", client0.strs().get("k1"));
     Assert.assertEquals("v1", client1.strs().get("k1"));
-    client.disconnect();
-    client1.disconnect();
-    MasterSalverTestUtil.stopAllProcess();
-    TimeUnit.SECONDS.sleep(1);
   }
 
-  @Test
-  public void testListPut() throws InterruptedException {
-    MasterSalverTestUtil.startAllProcess();
-    TimeUnit.SECONDS.sleep(5);
 
-    DstClient client = new DefaultDstClient(String.format("list://127.0.0.1:%d", 8082));
+  public void testListPut(DstClient client0, DstClient client1) {
 
-    client.lists().put("k1", ImmutableList.of("v1", "v2", "v3"));
-    Assert.assertEquals(ImmutableList.of("v1", "v2", "v3"),client.lists().get("k1"));
+    client0.lists().put("k1", ImmutableList.of("v1", "v2", "v3"));
+    Assert.assertEquals(ImmutableList.of("v1", "v2", "v3"), client0.lists().get("k1"));
     Assert.assertEquals(ImmutableList.of("v2", "v3"),
-        client.lists().get("k1", 1, 3));
+        client0.lists().get("k1", 1, 3));
 
-    DstClient client1 = new DefaultDstClient(String.format("list://127.0.0.1:%d", 8090));
-    Assert.assertEquals(ImmutableList.of("v1", "v2", "v3"),client1.lists().get("k1"));
-    client.disconnect();
-    client1.disconnect();
-    MasterSalverTestUtil.stopAllProcess();
-    TimeUnit.SECONDS.sleep(1);
+    Assert.assertEquals(ImmutableList.of("v1", "v2", "v3"), client1.lists().get("k1"));
   }
 
-  @Test
-  public void testSetPut() throws InterruptedException {
-    MasterSalverTestUtil.startAllProcess();
-    TimeUnit.SECONDS.sleep(5);
 
-    DstClient client = new DefaultDstClient(String.format("list://127.0.0.1:%d", 8082));
-
+  public void testSetPut(DstClient client0, DstClient client1) {
     Set<String> set = ImmutableSet.of("v1", "v2", "v3");
-    client.sets().put("k1", set);
-    Assert.assertEquals(set, client.sets().get("k1"));
-
-    DstClient client1 = new DefaultDstClient(String.format("list://127.0.0.1:%d", 8090));
+    client0.sets().put("k1", set);
+    Assert.assertEquals(set, client0.sets().get("k1"));
     Assert.assertEquals(set, client1.sets().get("k1"));
-    client.disconnect();
-    client1.disconnect();
-    MasterSalverTestUtil.stopAllProcess();
-    TimeUnit.SECONDS.sleep(1);
   }
 
-  @Test
-  public void testDictPut() throws InterruptedException {
-    MasterSalverTestUtil.startAllProcess();
-    TimeUnit.SECONDS.sleep(5);
-    DstClient client = new DefaultDstClient(String.format("list://127.0.0.1:%d", 8082));
 
+  public void testDictPut(DstClient client0, DstClient client1) {
     Map<String, String> dict = new HashMap<>();
     dict.put("k1", "v1");
-    client.dicts().put("m1", dict);
-    Map<String, String> dict1 = client.dicts().get("m1");
+    client0.dicts().put("m1", dict);
+    Map<String, String> dict1 = client0.dicts().get("m1");
     Assert.assertEquals(dict, dict1);
 
-    DstClient client1 = new DefaultDstClient(String.format("list://127.0.0.1:%d", 8090));
     Map<String, String> dict2 = client1.dicts().get("m1");
     Assert.assertEquals(dict, dict2);
-    client.disconnect();
-    client1.disconnect();
-    MasterSalverTestUtil.stopAllProcess();
-    TimeUnit.SECONDS.sleep(1);
   }
 
-  @Test
-  public void testSlistPut() throws InterruptedException {
-    MasterSalverTestUtil.startAllProcess();
-    TimeUnit.SECONDS.sleep(5);
-    DstClient client = new DefaultDstClient(String.format("list://127.0.0.1:%d", 8082));
 
+  public void testSlistPut(DstClient client0, DstClient client1) {
     LinkedList<SortedListEntity> list = new LinkedList<>();
     list.add(new SortedListEntity("xswl", 9));
     list.add(new SortedListEntity("wlll", 8));
     list.add(new SortedListEntity("fw", 9));
     list.add(new SortedListEntity("55", 6));
-    client.sortedLists().put("k1", list);
+    client0.sortedLists().put("k1", list);
 
-    LinkedList<SortedListEntity> tlist = client.sortedLists().top("k1", 100);
+    LinkedList<SortedListEntity> tlist = client0.sortedLists().top("k1", 100);
     Assert.assertEquals(tlist.get(0).getMember(), "fw");
-    Assert.assertEquals(tlist.get(1).getMember(),"xswl");
+    Assert.assertEquals(tlist.get(1).getMember(), "xswl");
 
-    DstClient client1 = new DefaultDstClient(String.format("list://127.0.0.1:%d", 8090));
     LinkedList<SortedListEntity> tlist1 = client1.sortedLists().top("k1", 100);
     Assert.assertEquals(tlist1.get(0).getMember(), "fw");
-    Assert.assertEquals(tlist1.get(1).getMember(),"xswl");
-
-    client.disconnect();
-    client1.disconnect();
-    MasterSalverTestUtil.stopAllProcess();
-    TimeUnit.SECONDS.sleep(1);
+    Assert.assertEquals(tlist1.get(1).getMember(), "xswl");
   }
 }
