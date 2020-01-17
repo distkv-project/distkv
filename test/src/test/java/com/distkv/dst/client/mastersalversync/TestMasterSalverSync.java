e0ac5278552ddac97856882ea5220ff50a19445e
kairbon <wangkai31415926@gmail.com>
2020-01-17 13:00:11 +0800

    fix

--- test/src/test/java/com/distkv/dst/client/mastersalversync/TestMasterSalverSync.java
+++ test/src/test/java/com/distkv/dst/client/mastersalversync/TestMasterSalverSync.java
@@ -8,7 +8,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
