5dfdd4f9ad03779502c05ebcfef658b87b7d8815
tortuo <tortuo369@gmail.com>
2020-01-18 16:45:33 +0800

    fix

---
+++ core/src/main/java/com/distkv/core/struct/slist/SortedListRBTreeImpl.java
@@ -0,0 +1,190 @@
+package com.distkv.core.struct.slist;
+
+import com.distkv.common.DstTuple;
+import com.distkv.common.entity.sortedList.SortedListEntity;
+
+import com.google.common.collect.TreeMultimap;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class SortedListRBTreeImpl
+    implements SortedList, java.io.Serializable {
+
+  private static final long serialVersionUID = -7011173289234009149L;
+
+  private TreeMultimap<Integer, String> scoreMemberMap;
+
+  private HashMap<String, Integer> memberScoreMap;
+
+  private static final Comparator<Integer> scoreComparator = (i1, i2) -> (i2 - i1);
+
+  private transient int size;
+
+  public SortedListRBTreeImpl() {
+    this.size = 0;
+    this.scoreMemberMap = TreeMultimap.create(scoreComparator, Comparator.naturalOrder());
+    this.memberScoreMap = new HashMap<>();
+  }
+
+  @Override
+  public int size() {
+    return size;
+  }
+
+  @Override
+  public boolean put(List<SortedListEntity> entities) {
+    if (hasDuplicatedMembers(entities)) {
+      return false;
+    }
+
+    for (SortedListEntity entity : entities) {
+      this.putItem(entity);
+    }
+    return true;
+  }
+
+  @Override
+  public void putItem(SortedListEntity entity) {
+    scoreMemberMap.put(entity.getScore(), entity.getMember());
+    memberScoreMap.put(entity.getMember(), entity.getScore());
+    size++;
+  }
+
+  @Override
+  public boolean removeItem(String member) {
+    if (!this.containsMember(member)) {
+      return false;
+    }
+
+    final Integer score = memberScoreMap.get(member);
+    scoreMemberMap.remove(score, member);
+    memberScoreMap.remove(member);
+    size--;
+    return true;
+  }
+
+  @Override
+  public int incrScore(String member, int delta) {
+    if (!this.containsMember(member)) {
+      return 0;
+    }
+
+    final int nowScore = memberScoreMap.get(member);
+    // Check if there is outing of range after increase the score:
+    //     Case 1: The score will more than Integer.MAX_VALUE when delta is positive
+    //     Case 2: The score will less than Integer.MIN_VALUE when delta is negative
+    if ((delta > 0 && nowScore > Integer.MAX_VALUE - delta) ||
+        (delta < 0 && nowScore < Integer.MIN_VALUE - delta)) {
+      return -1;
+    }
+
+    final int afterIncr = nowScore + delta;
+    scoreMemberMap.remove(nowScore, member);
+    memberScoreMap.remove(member);
+
+    scoreMemberMap.put(afterIncr, member);
+    memberScoreMap.put(member, afterIncr);
+    return 1;
+  }
+
+  @Override
+  public List<SortedListEntity> subList(int start, int end) {
+    final List<SortedListEntity> topList = new ArrayList<>();
+    int index = 1;
+    int nowRank = 1;
+    int lastRank = 1;
+    int lastScore = 1;
+    boolean isFirst = true;
+
+    for (final Entry<Integer, String> entry : scoreMemberMap.entries()) {
+      final String nowMember = entry.getValue();
+      final int nowScore = entry.getKey();
+
+      if (isFirst) {
+        isFirst = false;
+        nowRank = index;
+      } else {
+        lastRank = nowRank;
+        if (lastScore == nowScore) {
+          nowRank = lastRank;
+        } else {
+          nowRank = index;
+        }
+      }
+      lastScore = entry.getKey();
+
+      if (nowRank >= start && nowRank <= end) {
+        topList.add(new SortedListEntity(nowMember, nowScore));
+      }
+      if (nowRank > end) {
+        break;
+      }
+      index++;
+    }
+    return topList;
+  }
+
+  @Override
+  public DstTuple<Integer, Integer> getItem(String member) {
+    DstTuple<Integer, Integer> dstTuple = null;
+    if (containsMember(member)) {
+      int index = 1;
+      int nowRank = 1;
+      int lastRank = 1;
+      int lastScore = 1;
+      boolean isFirst = true;
+
+      for (final Entry<Integer, String> entry : scoreMemberMap.entries()) {
+        final String nowMember = entry.getValue();
+        final int nowScore = entry.getKey();
+
+        if (isFirst) {
+          isFirst = false;
+          nowRank = index;
+        } else {
+          lastRank = nowRank;
+          if (lastScore == nowScore) {
+            nowRank = lastRank;
+          } else {
+            nowRank = index;
+          }
+        }
+        lastScore = nowScore;
+
+        if (nowMember.equals(member)) {
+          dstTuple = new DstTuple<>(nowScore, nowRank);
+          break;
+        }
+        index++;
+      }
+    }
+    return dstTuple;
+  }
+
+  private boolean containsMember(String member) {
+    return memberScoreMap.containsKey(member);
+  }
+
+  /**
+   * Check whether the list value has duplicated members when initially put.
+   *
+   * @param sortedListEntities The list value
+   * @return True if the list value has duplicated members, otherwise return false.
+   */
+  private boolean hasDuplicatedMembers(
+      List<SortedListEntity> sortedListEntities) {
+    final Set<SortedListEntity> sortedListEntitySet = new HashSet<>();
+    for (final SortedListEntity e : sortedListEntities) {
+      if (sortedListEntitySet.contains(e)) {
+        return true;
+      }
+      sortedListEntitySet.add(e);
+    }
+    return false;
+  }
+}
---
+++ core/src/test/java/com/distkv/core/struct/slist/SortedListImplTest.java
@@ -0,0 +1,150 @@
+package com.distkv.core.struct.slist;
+
+import com.distkv.common.entity.sortedList.SortedListEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SortedListImplTest {
+
+  @Test
+  public static void testMain() {
+    testSortedListImpl(new SortedListRBTreeImpl());
+  }
+
+  public static void testSortedListImpl(SortedList sortedList) {
+    List<SortedListEntity> entities = new ArrayList<>();
+    entities.add(new SortedListEntity(String.valueOf(5), 5));
+    entities.add(new SortedListEntity(String.valueOf(9), 9));
+    entities.add(new SortedListEntity(String.valueOf(99), 9));
+    entities.add(new SortedListEntity(String.valueOf(7), 7));
+    entities.add(new SortedListEntity(String.valueOf(8), 8));
+    entities.add(new SortedListEntity(String.valueOf(88), 8));
+    entities.add(new SortedListEntity(String.valueOf(888), 8));
+
+    // To test put method
+    List<SortedListEntity> anothers = new ArrayList<>(entities);
+    anothers.add(new SortedListEntity(String.valueOf(7), 77));
+    // The list anothers has duplicated members, put failure
+    Assert.assertFalse(sortedList.put(anothers));
+    // Put the list entities success
+    Assert.assertTrue(sortedList.put(entities));
+
+    // To test size method
+    Assert.assertEquals(sortedList.size(), 7);
+
+    // To test getItem method
+    Assert.assertEquals(sortedList.getItem("5").getFirst().intValue(), 5);
+    Assert.assertEquals(sortedList.getItem("5").getSecond().intValue(), 7);
+    Assert.assertEquals(sortedList.getItem("9").getFirst().intValue(), 9);
+    Assert.assertEquals(sortedList.getItem("9").getSecond().intValue(), 1);
+    Assert.assertEquals(sortedList.getItem("99").getFirst().intValue(), 9);
+    Assert.assertEquals(sortedList.getItem("99").getSecond().intValue(), 1);
+    Assert.assertEquals(sortedList.getItem("7").getFirst().intValue(), 7);
+    Assert.assertEquals(sortedList.getItem("7").getSecond().intValue(), 6);
+    Assert.assertEquals(sortedList.getItem("8").getFirst().intValue(), 8);
+    Assert.assertEquals(sortedList.getItem("8").getSecond().intValue(), 3);
+    Assert.assertEquals(sortedList.getItem("88").getFirst().intValue(), 8);
+    Assert.assertEquals(sortedList.getItem("88").getSecond().intValue(), 3);
+    Assert.assertEquals(sortedList.getItem("888").getFirst().intValue(), 8);
+    Assert.assertEquals(sortedList.getItem("888").getSecond().intValue(), 3);
+    Assert.assertNull(sortedList.getItem("1234"));
+
+    // To test subList method
+    List<SortedListEntity> topList = sortedList.subList(1, 5);
+    Assert.assertEquals(topList.size(), 5);
+    Assert.assertEquals(topList.get(0).getMember(), "9");
+    Assert.assertEquals(topList.get(0).getScore(), 9);
+    Assert.assertEquals(topList.get(1).getMember(), "99");
+    Assert.assertEquals(topList.get(1).getScore(), 9);
+    Assert.assertEquals(topList.get(2).getMember(), "8");
+    Assert.assertEquals(topList.get(2).getScore(), 8);
+    Assert.assertEquals(topList.get(3).getMember(), "88");
+    Assert.assertEquals(topList.get(3).getScore(), 8);
+    Assert.assertEquals(topList.get(4).getMember(), "888");
+    Assert.assertEquals(topList.get(4).getScore(), 8);
+    topList = sortedList.subList(3, 3);
+    Assert.assertEquals(topList.size(), 3);
+    Assert.assertEquals(topList.get(0).getMember(), "8");
+    Assert.assertEquals(topList.get(0).getScore(), 8);
+    Assert.assertEquals(topList.get(1).getMember(), "88");
+    Assert.assertEquals(topList.get(1).getScore(), 8);
+    Assert.assertEquals(topList.get(2).getMember(), "888");
+    Assert.assertEquals(topList.get(2).getScore(), 8);
+
+    // To test putItem method
+    sortedList.putItem(new SortedListEntity(String.valueOf(2), 2));
+    Assert.assertEquals(sortedList.size(), 8);
+    Assert.assertEquals(sortedList.getItem("2").getFirst().intValue(), 2);
+    Assert.assertEquals(sortedList.getItem("2").getSecond().intValue(), 8);
+    Assert.assertEquals(sortedList.getItem("8").getFirst().intValue(), 8);
+    Assert.assertEquals(sortedList.getItem("8").getSecond().intValue(), 3);
+    sortedList.putItem(new SortedListEntity(String.valueOf(444), 4));
+    Assert.assertEquals(sortedList.size(), 9);
+    Assert.assertEquals(sortedList.getItem("2").getFirst().intValue(), 2);
+    Assert.assertEquals(sortedList.getItem("2").getSecond().intValue(), 9);
+    Assert.assertEquals(sortedList.getItem("8").getFirst().intValue(), 8);
+    Assert.assertEquals(sortedList.getItem("8").getSecond().intValue(), 3);
+    Assert.assertEquals(sortedList.getItem("444").getFirst().intValue(), 4);
+    Assert.assertEquals(sortedList.getItem("444").getSecond().intValue(), 8);
+    sortedList.putItem(new SortedListEntity(String.valueOf(10), 10));
+    Assert.assertEquals(sortedList.size(), 10);
+    Assert.assertEquals(sortedList.getItem("10").getFirst().intValue(), 10);
+    Assert.assertEquals(sortedList.getItem("10").getSecond().intValue(), 1);
+    Assert.assertEquals(sortedList.getItem("8").getFirst().intValue(), 8);
+    Assert.assertEquals(sortedList.getItem("8").getSecond().intValue(), 4);
+
+    // To test removeItem method
+    Assert.assertFalse(sortedList.removeItem("333"));
+    Assert.assertTrue(sortedList.removeItem("88"));
+    Assert.assertEquals(sortedList.size(), 9);
+    Assert.assertNull(sortedList.getItem("88"));
+    Assert.assertEquals(sortedList.getItem("7").getFirst().intValue(), 7);
+    Assert.assertEquals(sortedList.getItem("7").getSecond().intValue(), 6);
+    Assert.assertEquals(sortedList.getItem("888").getFirst().intValue(), 8);
+    Assert.assertEquals(sortedList.getItem("888").getSecond().intValue(), 4);
+    Assert.assertTrue(sortedList.removeItem("2"));
+    Assert.assertEquals(sortedList.size(), 8);
+    Assert.assertNull(sortedList.getItem("2"));
+    Assert.assertEquals(sortedList.getItem("7").getFirst().intValue(), 7);
+    Assert.assertEquals(sortedList.getItem("7").getSecond().intValue(), 6);
+    Assert.assertEquals(sortedList.getItem("888").getFirst().intValue(), 8);
+    Assert.assertEquals(sortedList.getItem("888").getSecond().intValue(), 4);
+
+    // To test incrScore method
+    Assert.assertEquals(sortedList.incrScore("99", Integer.MAX_VALUE), -1);
+    sortedList.putItem(new SortedListEntity(String.valueOf(-1), -1));
+    Assert.assertEquals(sortedList.incrScore("-1", Integer.MIN_VALUE), -1);
+    Assert.assertEquals(sortedList.incrScore("123456", 123456), 0);
+    Assert.assertEquals(sortedList.incrScore("99", -4), 1);
+    Assert.assertEquals(sortedList.size(), 9);
+    Assert.assertEquals(sortedList.getItem("9").getFirst().intValue(), 9);
+    Assert.assertEquals(sortedList.getItem("9").getSecond().intValue(), 2);
+    Assert.assertEquals(sortedList.getItem("5").getFirst().intValue(), 5);
+    Assert.assertEquals(sortedList.getItem("5").getSecond().intValue(), 6);
+    Assert.assertEquals(sortedList.getItem("99").getFirst().intValue(), 5);
+    Assert.assertEquals(sortedList.getItem("99").getSecond().intValue(), 6);
+    Assert.assertEquals(sortedList.getItem("-1").getFirst().intValue(), -1);
+    Assert.assertEquals(sortedList.getItem("-1").getSecond().intValue(), 9);
+    Assert.assertEquals(sortedList.incrScore("7", 3), 1);
+    Assert.assertEquals(sortedList.size(), 9);
+    Assert.assertEquals(sortedList.getItem("10").getFirst().intValue(), 10);
+    Assert.assertEquals(sortedList.getItem("10").getSecond().intValue(), 1);
+    Assert.assertEquals(sortedList.getItem("7").getFirst().intValue(), 10);
+    Assert.assertEquals(sortedList.getItem("7").getSecond().intValue(), 1);
+    Assert.assertEquals(sortedList.getItem("888").getFirst().intValue(), 8);
+    Assert.assertEquals(sortedList.getItem("888").getSecond().intValue(), 4);
+    Assert.assertEquals(sortedList.getItem("444").getFirst().intValue(), 4);
+    Assert.assertEquals(sortedList.getItem("444").getSecond().intValue(), 8);
+  }
+
+  public static void printSortedList(SortedList sortedList) {
+    System.out.println("********************");
+    System.out.println(sortedList.size());
+    List<SortedListEntity> topList = sortedList.subList(1, sortedList.size());
+    topList.forEach(entity -> System.out.println(entity.getMember() + ": " + entity.getScore()));
+  }
+
+}
