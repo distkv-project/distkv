663091f695ce204d3cfb952ea354237e12ffc166
meijies <meijie.work@gmail.com>
2020-02-08 20:06:03 +0800

    modify the variable name

--- scripts/uninstall_distkv.sh
+++ scripts/uninstall_distkv.sh
@@ -3,9 +3,9 @@
 set -e
 set -x
 
-DKV_PID_FIle=/var/lib/dkv/dkv-server.currentpid
-if [ -f "$DKV_PID_FIle" ]; then
-  DKV_PID=$(cat "$DKV_PID_FIle")
+DKV_PID_FILe=/var/lib/dkv/dkv-server.currentpid
+if [ -f "$DKV_PID_FILe" ]; then
+  DKV_PID=$(cat "$DKV_PID_FILe")
 fi
 
 if [ -n "$DKV_PID" ] && sudo kill -0 "$DKV_PID" > /dev/null 2>&1; then
