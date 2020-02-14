c62790a50b0babed5529b20a2a12c75ce09dfba0
Qing Wang <kingchin1218@gmail.com>
2020-02-14 21:13:42 +0800

    Make SyncClient wrap the AsyncClient. (#506)
    * Finish str sync client.
    
    * Finish list proxy
    
    * Finish set proxy
    
    * Finish dict sortedlist proxies
    
    * Fix linting

--- client/src/main/java/com/distkv/client/DistkvDictProxy.java
+++ client/src/main/java/com/distkv/client/DistkvDictProxy.java
@@ -1,52 +1,37 @@
 package com.distkv.client;
 
+import com.distkv.asyncclient.DistkvAsyncDictProxy;
 import com.distkv.common.exception.DistkvException;
 import com.distkv.common.utils.FutureUtils;
 import com.distkv.rpc.protobuf.generated.DictProtocol;
 import com.distkv.rpc.protobuf.generated.DictProtocol.DictGetItemResponse;
 import com.distkv.rpc.protobuf.generated.DictProtocol.DictGetResponse;
 import com.distkv.rpc.protobuf.generated.DictProtocol.DictPopItemResponse;
-import com.distkv.rpc.protobuf.generated.DistkvProtocol.DistkvRequest;
 import com.distkv.rpc.protobuf.generated.DistkvProtocol.DistkvResponse;
-import com.distkv.rpc.protobuf.generated.DistkvProtocol.RequestType;
-import com.distkv.rpc.service.DistkvService;
-import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DistkvDictProxy {
 
-  private DistkvService service;
+  private static final String typeCode = "D";
 
-  private String typeCode = "D";
+  private DistkvAsyncDictProxy asyncDictProxy;
 
-  public DistkvDictProxy(DistkvService service) {
-    this.service = service;
+  public DistkvDictProxy(DistkvAsyncDictProxy asyncDictProxy) {
+    this.asyncDictProxy = asyncDictProxy;
   }
 
   // Put a new dict.
   public void put(String key, Map<String, String> dict) {
-    DictProtocol.DictPutRequest.Builder dictPutRequest = DictProtocol.DictPutRequest.newBuilder();
-    DictProtocol.DistKVDict distKVDict = DictUtil.buildDistKVDict(dict);
-    dictPutRequest.setDict(distKVDict);
-    DistkvRequest request = DistkvRequest.newBuilder()
-        .setKey(key)
-        .setRequestType(RequestType.DICT_PUT)
-        .setRequest(Any.pack(dictPutRequest.build()))
-        .build();
-    DistkvResponse response = FutureUtils.get(service.call(request));
-    CheckStatusUtil.checkStatus(response.getStatus(), request.getKey(), typeCode);
+    DistkvResponse response = FutureUtils.get(asyncDictProxy.put(key, dict));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
   }
 
   // Get a dict.
   public Map<String, String> get(String key) {
-    DistkvRequest request = DistkvRequest.newBuilder()
-        .setKey(key)
-        .setRequestType(RequestType.DICT_GET)
-        .build();
-    DistkvResponse response = FutureUtils.get(service.call(request));
-    CheckStatusUtil.checkStatus(response.getStatus(), request.getKey(), typeCode);
+    DistkvResponse response = FutureUtils.get(asyncDictProxy.get(key));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
     DictProtocol.DistKVDict distKVDict = null;
     try {
       distKVDict = response.getResponse().unpack(DictGetResponse.class).getDict();
@@ -62,16 +47,8 @@ public class DistkvDictProxy {
 
   // Get the value in the dict corresponding to the key.
   public String getItem(String key, String itemKey) {
-    DictProtocol.DictGetItemRequest.Builder dictGetItemRequest = DictProtocol.DictGetItemRequest
-        .newBuilder()
-        .setItemKey(itemKey);
-    DistkvRequest request = DistkvRequest.newBuilder()
-        .setKey(key)
-        .setRequestType(RequestType.DICT_GET_ITEM)
-        .setRequest(Any.pack(dictGetItemRequest.build()))
-        .build();
-    DistkvResponse response = FutureUtils.get(service.call(request));
-    CheckStatusUtil.checkStatus(response.getStatus(), request.getKey(), typeCode);
+    DistkvResponse response = FutureUtils.get(asyncDictProxy.getItem(key, itemKey));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
     try {
       return response.getResponse().unpack(DictGetItemResponse.class).getItemValue();
     } catch (InvalidProtocolBufferException e) {
@@ -81,16 +58,8 @@ public class DistkvDictProxy {
 
   // Pop the item in the dict corresponding to the key.
   public String popItem(String key, String itemKey) {
-    DictProtocol.DictPopItemRequest.Builder dictPopItemRequest = DictProtocol.DictPopItemRequest
-        .newBuilder()
-        .setItemKey(itemKey);
-    DistkvRequest request = DistkvRequest.newBuilder()
-        .setKey(key)
-        .setRequestType(RequestType.DICT_POP_ITEM)
-        .setRequest(Any.pack(dictPopItemRequest.build()))
-        .build();
-    DistkvResponse response = FutureUtils.get(service.call(request));
-    CheckStatusUtil.checkStatus(response.getStatus(), request.getKey(), typeCode);
+    DistkvResponse response = FutureUtils.get(asyncDictProxy.popItem(key, itemKey));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
     try {
       return response.getResponse().unpack(DictPopItemResponse.class).getItemValue();
     } catch (InvalidProtocolBufferException e) {
@@ -100,17 +69,8 @@ public class DistkvDictProxy {
 
   // Put the item in the dict corresponding to the key.
   public void putItem(String key, String itemKey, String itemValue) {
-    DictProtocol.DictPutItemRequest.Builder dictPutItemRequest = DictProtocol.DictPutItemRequest
-        .newBuilder()
-        .setItemKey(itemKey)
-        .setItemValue(itemValue);
-    DistkvRequest request = DistkvRequest.newBuilder()
-        .setKey(key)
-        .setRequestType(RequestType.DICT_PUT_ITEM)
-        .setRequest(Any.pack(dictPutItemRequest.build()))
-        .build();
-    DistkvResponse response = FutureUtils.get(service.call(request));
-    CheckStatusUtil.checkStatus(response.getStatus(), request.getKey(), typeCode);
+    DistkvResponse response = FutureUtils.get(asyncDictProxy.putItem(key, itemKey, itemValue));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
   }
 
   /**
@@ -119,26 +79,13 @@ public class DistkvDictProxy {
    * @param key The key to be dropped.
    */
   public void drop(String key) {
-    DistkvRequest request = DistkvRequest.newBuilder()
-        .setKey(key)
-        .setRequestType(RequestType.DICT_DROP)
-        .build();
-    DistkvResponse response = FutureUtils.get(service.call(request));
-    CheckStatusUtil.checkStatus(response.getStatus(), request.getKey(), typeCode);
+    DistkvResponse response = FutureUtils.get(asyncDictProxy.drop(key));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
   }
 
   // Remove the item in the dict corresponding to the key.
   public void removeItem(String key, String itemKey) {
-    DictProtocol.DictRemoveItemRequest.Builder dictRemoveItemRequest =
-        DictProtocol.DictRemoveItemRequest
-            .newBuilder()
-            .setItemKey(itemKey);
-    DistkvRequest request = DistkvRequest.newBuilder()
-        .setKey(key)
-        .setRequestType(RequestType.DICT_REMOVE_ITEM)
-        .setRequest(Any.pack(dictRemoveItemRequest.build()))
-        .build();
-    DistkvResponse response = FutureUtils.get(service.call(request));
-    CheckStatusUtil.checkStatus(response.getStatus(), request.getKey(), typeCode);
+    DistkvResponse response = FutureUtils.get(asyncDictProxy.removeItem(key, itemKey));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
   }
 }
