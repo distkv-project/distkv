ac5b27389ad2a3d999b85c9c9ec1917dd07a77ed
jovany-wang <kingchin1218@126.com>
2020-02-14 20:47:28 +0800

    Finish dict sortedlist proxies

--- client/src/main/java/com/distkv/client/DistkvDictProxy.java
+++ client/src/main/java/com/distkv/client/DistkvDictProxy.java
@@ -1,48 +1,76 @@
 package com.distkv.client;
 
+import com.distkv.asyncclient.DistkvAsyncDictProxy;
 import com.distkv.common.exception.DistkvException;
 import com.distkv.common.utils.FutureUtils;
 import com.distkv.rpc.protobuf.generated.DictProtocol;
 import com.distkv.rpc.protobuf.generated.DictProtocol.DictGetItemResponse;
 import com.distkv.rpc.protobuf.generated.DictProtocol.DictGetResponse;
 import com.distkv.rpc.protobuf.generated.DictProtocol.DictPopItemResponse;
-import com.distkv.rpc.protobuf.generated.DistkvProtocol.DistkvRequest;
 import com.distkv.rpc.protobuf.generated.DistkvProtocol.DistkvResponse;
-import com.distkv.rpc.protobuf.generated.DistkvProtocol.RequestType;
-import com.distkv.rpc.service.DistkvService;
-import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class DistkvDictProxy {
 
-  private DistkvService service;
+  private static final String typeCode = "D";
 
-  private String typeCode = "D";
+  private DistkvAsyncDictProxy asyncDictProxy;
 
-  public DistkvDictProxy(DistkvService service) {
-    this.service = service;
+  public DistkvDictProxy(DistkvAsyncDictProxy asyncDictProxy) {
+    this.asyncDictProxy = asyncDictProxy;
   }
 
   // Put a new dict.
   public void put(String key, Map<String, String> dict) {
+    DistkvResponse response = FutureUtils.get(asyncDictProxy.put(key, dict));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
   }
 
   // Get a dict.
   public Map<String, String> get(String key) {
+    DistkvResponse response = FutureUtils.get(asyncDictProxy.get(key));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
+    DictProtocol.DistKVDict distKVDict = null;
+    try {
+      distKVDict = response.getResponse().unpack(DictGetResponse.class).getDict();
+    } catch (InvalidProtocolBufferException e) {
+      throw new DistkvException(e.toString());
+    }
+    Map<String, String> dict = new HashMap<>();
+    for (int i = 0; i < distKVDict.getKeysCount(); i++) {
+      dict.put(distKVDict.getKeys(i), distKVDict.getValues(i));
+    }
+    return dict;
   }
 
   // Get the value in the dict corresponding to the key.
   public String getItem(String key, String itemKey) {
+    DistkvResponse response = FutureUtils.get(asyncDictProxy.getItem(key, itemKey));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
+    try {
+      return response.getResponse().unpack(DictGetItemResponse.class).getItemValue();
+    } catch (InvalidProtocolBufferException e) {
+      throw new DistkvException(e.toString());
+    }
   }
 
   // Pop the item in the dict corresponding to the key.
   public String popItem(String key, String itemKey) {
+    DistkvResponse response = FutureUtils.get(asyncDictProxy.popItem(key, itemKey));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
+    try {
+      return response.getResponse().unpack(DictPopItemResponse.class).getItemValue();
+    } catch (InvalidProtocolBufferException e) {
+      throw new DistkvException(e.toString());
+    }
   }
 
   // Put the item in the dict corresponding to the key.
   public void putItem(String key, String itemKey, String itemValue) {
+    DistkvResponse response = FutureUtils.get(asyncDictProxy.putItem(key, itemKey, itemValue));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
   }
 
   /**
@@ -51,10 +79,13 @@ public class DistkvDictProxy {
    * @param key The key to be dropped.
    */
   public void drop(String key) {
+    DistkvResponse response = FutureUtils.get(asyncDictProxy.drop(key));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
   }
 
   // Remove the item in the dict corresponding to the key.
   public void removeItem(String key, String itemKey) {
-
+    DistkvResponse response = FutureUtils.get(asyncDictProxy.removeItem(key, itemKey));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
   }
 }
--- client/src/main/java/com/distkv/client/DistkvSortedListProxy.java
+++ client/src/main/java/com/distkv/client/DistkvSortedListProxy.java
@@ -1,54 +1,72 @@
 package com.distkv.client;
 
+import com.distkv.asyncclient.DistkvAsyncSortedListProxy;
 import com.distkv.common.DistkvTuple;
 import com.distkv.common.entity.sortedList.SortedListEntity;
 import com.distkv.common.utils.FutureUtils;
-import com.distkv.rpc.protobuf.generated.DistkvProtocol.DistkvRequest;
 import com.distkv.rpc.protobuf.generated.DistkvProtocol.DistkvResponse;
-import com.distkv.rpc.protobuf.generated.DistkvProtocol.RequestType;
 import com.distkv.rpc.protobuf.generated.SortedListProtocol;
 import com.distkv.rpc.protobuf.generated.SortedListProtocol.SlistGetMemberResponse;
-import com.distkv.rpc.protobuf.generated.SortedListProtocol.SlistPutRequest;
 import com.distkv.rpc.protobuf.generated.SortedListProtocol.SlistTopResponse;
-import com.distkv.rpc.service.DistkvService;
-import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
 import java.util.LinkedList;
-import java.util.concurrent.CompletableFuture;
+
 
 public class DistkvSortedListProxy {
 
-  private String typeCode = "E";
+  private static final String typeCode = "E";
 
-  public DistkvSortedListProxy(DistkvService service) {
-    this.service = service;
+  private DistkvAsyncSortedListProxy asyncSortedListProxy;
+
+  public DistkvSortedListProxy(DistkvAsyncSortedListProxy asyncSortedListProxy) {
+    this.asyncSortedListProxy = asyncSortedListProxy;
   }
 
   public void put(String key, LinkedList<SortedListEntity> list) {
+    DistkvResponse response = FutureUtils.get(asyncSortedListProxy.put(key, list));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
   }
 
   public void incrScore(String key, String member, int delta) {
 
+    DistkvResponse response = FutureUtils.get(asyncSortedListProxy.incrScore(key, member, delta));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
   }
 
   public LinkedList<SortedListEntity> top(String key, int topNum)
       throws InvalidProtocolBufferException {
-
+    DistkvResponse response = FutureUtils.get(asyncSortedListProxy.top(key, topNum));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
+    LinkedList<SortedListEntity> list = new LinkedList<>();
+    SlistTopResponse slistTopResponse = response.getResponse().unpack(SlistTopResponse.class);
+    for (SortedListProtocol.SortedListEntity entity : slistTopResponse.getListList()) {
+      list.add(new SortedListEntity(entity.getMember(), entity.getScore()));
+    }
+    return list;
   }
 
   public void drop(String key) {
-
+    DistkvResponse response = FutureUtils.get(asyncSortedListProxy.drop(key));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
   }
 
   public void removeMember(String key, String member) {
-
+    DistkvResponse response = FutureUtils.get(asyncSortedListProxy.removeMember(key, member));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
   }
 
   public void putMember(String key, SortedListEntity entity) {
-
+    DistkvResponse response = FutureUtils.get(asyncSortedListProxy.putMember(key, entity));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
   }
 
   public DistkvTuple<Integer, Integer> getMember(String key, String member)
       throws InvalidProtocolBufferException {
+    DistkvResponse response = FutureUtils.get(asyncSortedListProxy.getMember(key, member));
+    CheckStatusUtil.checkStatus(response.getStatus(), key, typeCode);
+    SlistGetMemberResponse slistGetMemberResponse = response.getResponse()
+        .unpack(SlistGetMemberResponse.class);
+    SortedListProtocol.SortedListEntity sortedListEntity = slistGetMemberResponse.getEntity();
+    return new DistkvTuple<>(sortedListEntity.getScore(), slistGetMemberResponse.getCount());
   }
 }
