1179795d9a75d15b43008c24de19d4cb8b37e5f2
Tuo Wang <tuowang@TuodeMBP.lan>
2020-02-25 19:38:26 +0800

    Fix

--- client/src/main/java/com/distkv/client/DistkvClient.java
+++ client/src/main/java/com/distkv/client/DistkvClient.java
@@ -1,28 +1,27 @@
 package com.distkv.client;
 
-import com.distkv.rpc.protobuf.generated.DistkvProtocol.DistkvRequest;
 
 /**
- * The DstClient class is used to connect to the dst server.
+ * The DistkvClient class is used to connect to the distkv server.
  */
 public interface DistkvClient {
 
   /**
-   * Connect to dst server.
+   * Connect to distkv server.
    *
    * @return True if succeeded to connect, otherwise it's false.
    */
   boolean connect();
 
   /**
-   * Whether the client connected to dst server.
+   * Whether the client connected to distkv server.
    *
    * @return True if it connected, otherwise it's false.
    */
   boolean isConnected();
 
   /**
-   * Disconnect to dst server.
+   * Disconnect to distkv server.
    */
   boolean disconnect();
 
@@ -43,45 +42,46 @@ public interface DistkvClient {
   String getActivedNamespace();
 
   /**
-   * Get the dst string proxy.
+   * Get the distkv string proxy.
    *
-   * @return The dst string proxy.
+   * @return The distkv string proxy.
    */
   DistkvStringProxy strs();
 
 
   /**
-   * Get the dst dict proxy.
+   * Get the distkv dict proxy.
    *
-   * @return The dst string proxy.
+   * @return The distkv string proxy.
    */
   DistkvDictProxy dicts();
 
   /**
-   * Get the dst list proxy.
+   * Get the distkv list proxy.
    *
-   * @return The dst list proxy.
+   * @return The distkv list proxy.
    */
   DistkvListProxy lists();
 
   /**
-   * Get the dst set proxy.
+   * Get the distkv set proxy.
    *
-   * @return The dst set proxy.
+   * @return The distkv set proxy.
    */
   DistkvSetProxy sets();
 
   /**
-   * Get the dst sortedList proxy.
+   * Get the distkv sortedList proxy.
    *
-   * @return The dst sortedList proxy.
+   * @return The distkv sortedList proxy.
    */
   DistkvSortedListProxy sortedLists();
+
 
   /**
    * Get the distkv int proxy
    *
    * @return The distkv int proxy
    */
-
+  DistkvIntProxy ints();
 }
