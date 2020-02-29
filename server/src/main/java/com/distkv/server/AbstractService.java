fda04ea1650d7c9737ec7e5a94251eb41c987c4e
meijies <meijie.work@gmail.com>
2020-02-29 19:24:10 +0800

    fix checkstyle

--- server/src/main/java/com/distkv/server/AbstractService.java
+++ server/src/main/java/com/distkv/server/AbstractService.java
@@ -3,8 +3,6 @@ package com.distkv.server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-
 public abstract class AbstractService implements Service {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(AbstractService.class);
