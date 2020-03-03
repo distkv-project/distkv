8ed3ac197c6d9cfc72af7ec9bb00e09c4dbed14b
senyer <576085903@qq.com>
2020-03-03 20:23:50 +0800

    Fix

--- test/src/test/java/com/distkv/supplier/MasterSlaveSyncTestUtil.java
+++ test/src/test/java/com/distkv/supplier/MasterSlaveSyncTestUtil.java
@@ -2,6 +2,8 @@ package com.distkv.supplier;
 
 import com.distkv.server.storeserver.StoreConfig;
 import com.distkv.server.storeserver.StoreServer;
+import com.sun.xml.internal.bind.v2.TODO;
+import java.io.File;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class MasterSlaveSyncTestUtil {
@@ -13,15 +15,18 @@ public class MasterSlaveSyncTestUtil {
   private static final int NODE_NUM = 3;
 
   public static void startAllProcess() {
+    final File userDir = new File(System.getProperty("user.dir"));
+    String confPath = userDir.getParent() + File.separator + "test" +
+        File.separator + "conf" + File.separator;
     for (int i = 0; i < NODE_NUM - 1; i++) {
-      System.setProperty(KEY_PROPERTY, "slave_store_" + (i + 1) + ".conf");
+      System.setProperty(KEY_PROPERTY, confPath + "slave_store_" + (i + 1) + ".conf");
       StoreConfig config = StoreConfig.create();
       StoreServer storeServer = new StoreServer(config);
       storeServer.run();
       storeServers.add(storeServer);
     }
 
-    System.setProperty(KEY_PROPERTY, "master_store.conf");
+    System.setProperty(KEY_PROPERTY, confPath + "master_store.conf");
     StoreConfig config = StoreConfig.create();
     StoreServer storeServer = new StoreServer(config);
     storeServer.run();
@@ -30,7 +35,7 @@ public class MasterSlaveSyncTestUtil {
 
   public static void stopAllProcess() {
     for (int i = 0; i < NODE_NUM; i++) {
-
+      storeServers.get(i).shutdown();
     }
   }
 }
