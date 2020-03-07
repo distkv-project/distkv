01950a70c9913030a051a1ec1923ac08cefe3b67
tortuo <tortuo369@gmail.com>
2020-03-07 21:04:30 +0800

    fix

--- pine/src/main/java/com/distkv/pine/components/liker/PineLiker.java
+++ pine/src/main/java/com/distkv/pine/components/liker/PineLiker.java
@@ -1,14 +1,14 @@
 package com.distkv.pine.components.liker;
 
 /**
- * The `PineLiker` component is used to like the other comment
+ * The `PineLiker` component is used to like the other comment.
  */
 public interface PineLiker {
 
   /**
-   * Get the people set who is liking the topic
-   * @param topic the topic name
-   * @return the people set
+   * Get the people set who is liking the topic.
+   * @param topic the topic name.
+   * @return the people set.
    */
   public PipeLikerTopic getTopic(String topic);
 
