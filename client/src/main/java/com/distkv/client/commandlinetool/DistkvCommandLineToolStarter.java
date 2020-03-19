ed84d419d06417ff16de2eb76cbd103fe3d11848
sunyang <503386110@qq.com>
2020-03-19 13:04:14 +0800

    fix

--- client/src/main/java/com/distkv/client/commandlinetool/DistkvCommandLineToolStarter.java
+++ client/src/main/java/com/distkv/client/commandlinetool/DistkvCommandLineToolStarter.java
@@ -1,5 +1,6 @@
 package com.distkv.client.commandlinetool;
 
+import java.io.IOException;
 import java.util.Scanner;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
@@ -14,6 +15,15 @@ import com.distkv.common.exception.SortedListMemberNotFoundException;
 import com.distkv.common.exception.SortedListTopNumIsNonNegativeException;
 import com.distkv.parser.DistkvParser;
 import com.distkv.parser.po.DistkvParsedResult;
+import org.jline.reader.Completer;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.impl.completer.AggregateCompleter;
+import org.jline.reader.impl.completer.ArgumentCompleter;
+import org.jline.reader.impl.completer.NullCompleter;
+import org.jline.reader.impl.completer.StringsCompleter;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 
 public class DistkvCommandLineToolStarter {
 
@@ -24,15 +34,15 @@ public class DistkvCommandLineToolStarter {
   private static final String DEFAULT_VERSION = "0.1.0";
 
   @Parameter(names = {"--help", "-help"}, description = "Show help messages.",
-          help = true, order = -1)
+      help = true, order = -1)
   private static boolean HELP = false;
 
   @Parameter(names = {"--address"}, description = "Specify the address of server to connect.",
-          order = 1)
+      order = 1)
   private static String ADDRESS = "127.0.0.1:8082";
 
   @Parameter(names = {"-v", "-version", "-V"}, description = "Show the version of Dst.",
-          help = true, order = 2)
+      help = true, order = 2)
   private static boolean VERSION = false;
 
 
@@ -65,38 +75,39 @@ public class DistkvCommandLineToolStarter {
       distkvClient = new DefaultDistkvClient(String.format("distkv://%s", ADDRESS));
     } catch (Exception e) {
       System.out.println(String.format("Failed to connect to dst server, %s, "
-              + "please check your input.", ADDRESS));
+          + "please check your input.", ADDRESS));
       return;
     }
-    new DistkvCommandLineToolStarter().loop(distkvClient);
+    try  {
+      new DistkvCommandLineToolStarter().loop(distkvClient);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
-  private void loop(DistkvClient distkvClient) {
-    DistkvParser distkvParser = new DistkvParser();
-    DistkvCommandExecutor distkvCommandExecutor = new DistkvCommandExecutor(distkvClient);
-    Scanner sc = new Scanner(System.in);
+  private void loop(DistkvClient distkvClient) throws IOException {
+
+    Completer strPutCompleter = new ArgumentCompleter(
+        new StringsCompleter("str.put"),
+        new StringsCompleter("*"),
+        new StringsCompleter("*"),
+        NullCompleter.INSTANCE );
+    Completer strGetCompleter = new ArgumentCompleter(
+        new StringsCompleter("str.get"),
+        new StringsCompleter("*"),
+        new StringsCompleter("*"),
+        NullCompleter.INSTANCE );
+    Completer allCompleters = new AggregateCompleter(strPutCompleter, strGetCompleter);
+
+    Terminal terminal = TerminalBuilder.builder().system(true).build();
+    LineReader lineReader = LineReaderBuilder.builder().terminal(terminal).completer(allCompleters).build();
+    final String prompt = "dst-cli >";
     while (true) {
-      System.out.print(PROMPT_STRING);
-      final String command = sc.nextLine();
       String result = null;
-      try {
-        DistkvParsedResult parsedResult = distkvParser.parse(command);
-        result = distkvCommandExecutor.execute(parsedResult);
-      } catch (DictKeyNotFoundException e) {
-        result = ("errorCode: " + e.getErrorCode() + ";\n Detail: " + e.getMessage());
-      } catch (DistkvListIndexOutOfBoundsException e) {
-        result = ("errorCode: " + e.getErrorCode() + ";\n Detail: " + e.getMessage());
-      } catch (KeyNotFoundException e) {
-        result = ("errorCode: " + e.getErrorCode() + ";\n Detail: " + e.getMessage());
-      } catch (SortedListMemberNotFoundException e) {
-        result = ("errorCode: " + e.getErrorCode() + ";\n Detail: " + e.getMessage());
-      } catch (SortedListTopNumIsNonNegativeException e) {
-        result = ("errorCode: " + e.getErrorCode() + ";\n Detail: " + e.getMessage());
-      } catch (DistkvException e) {
-        result = ("errorCode: " + e.getErrorCode() + ";\n Detail: " + e.getMessage());
-      }
-      System.out.println(PROMPT_STRING + result);
+      String line;
+      line = lineReader.readLine(prompt);
     }
+
   }
 
 }
