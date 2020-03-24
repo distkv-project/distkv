e9d86d5f89344a599ec8c0354e5fefc1b1ec0ca0
senyer <576085903@qq.com>
2020-03-25 00:20:10 +0800

    up cov

--- common/src/main/java/com/distkv/common/exception/DistkvException.java
+++ common/src/main/java/com/distkv/common/exception/DistkvException.java
@@ -14,20 +14,7 @@ public class DistkvException extends RuntimeException {
     this.errorCode = errorCode;
   }
 
-  public DistkvException(String errorCode, String errorMessage, Exception e) {
-    super(errorMessage, e);
-    this.errorCode = errorCode;
-  }
-
   public DistkvException(String errorMessage) {
     super(errorMessage);
-  }
-
-  public DistkvException(String errorMessage, Exception e) {
-    super(errorMessage, e);
-  }
-
-  public String getErrorCode() {
-    return errorCode;
   }
 }
