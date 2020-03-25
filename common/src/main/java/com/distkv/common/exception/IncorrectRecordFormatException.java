8335f9630d6694d67781f29cc4e0dc6be2778697
senyer <37206562+senyer@users.noreply.github.com>
2020-03-25 22:05:22 +0800

    Clean up useless custom exception. (#640)
    * Add Change log
    
    * Fix
    
    * Fix
    
    * Remove useless custom exception.

--- common/src/main/java/com/distkv/common/exception/IncorrectRecordFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.distkv.common.exception;
-
-public class IncorrectRecordFormatException extends DistkvException {
-  public IncorrectRecordFormatException(String tableName) {
-    super(String.format("Incorrect record format of table %s", tableName));
-  }
-}
