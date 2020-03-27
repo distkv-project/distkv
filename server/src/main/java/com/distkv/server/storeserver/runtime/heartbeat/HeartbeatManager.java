9e9d452094208c089a1b01b372edc74a94ca9deb
karbon <wangkai31415926@gmail.com>
2020-03-27 17:45:38 +0800

    fix

--- server/src/main/java/com/distkv/server/storeserver/runtime/heartbeat/HeartbeatManager.java
+++ server/src/main/java/com/distkv/server/storeserver/runtime/heartbeat/HeartbeatManager.java
@@ -12,7 +12,6 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class HeartbeatManager {
 
