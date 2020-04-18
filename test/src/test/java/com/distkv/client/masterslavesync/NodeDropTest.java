557a47aa27b2a3248a19baa346ccbff3e3a5da1e
jovany-wang <kingchin1218@126.com>
2020-04-18 14:22:39 +0800

    Fix

--- test/src/test/java/com/distkv/client/masterslavesync/NodeDropTest.java
+++ test/src/test/java/com/distkv/client/masterslavesync/NodeDropTest.java
@@ -48,7 +48,7 @@ public class NodeDropTest {
     }
   }
 
-  @Test
+  @Test(singleThreaded=true)
   public void testSingleStoreServerDead() {
     try {
       DmetaTestUtil.startAllMetaServerProcesses();
